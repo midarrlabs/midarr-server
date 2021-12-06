5ba09dac0b5777119d2e059dd82bab1d2779285b
trueChazza <cgnetwork.nz@gmail.com>
2021-12-06 18:01:39 +1300

    feat(common): add latest file metadata

--- lib/media_server_web/controllers/stream_controller.ex
+++ lib/media_server_web/controllers/stream_controller.ex
@@ -2,7 +2,7 @@ defmodule MediaServerWeb.StreamController do
   use MediaServerWeb, :controller
 
   alias MediaServer.Media
-
+  
   def show(%{req_headers: headers} = conn, %{"id" => id}) do
 
     file = Media.get_file!(id)
@@ -10,29 +10,28 @@ defmodule MediaServerWeb.StreamController do
     send_video(conn, headers, file.path)
   end
 
-    defp get_offset(headers) do
-      case List.keyfind(headers, "range", 0) do
-        {"range", "bytes=" <> start_pos} ->
-          String.split(start_pos, "-") |> hd |> String.to_integer
-        nil ->
-          0
-      end
+  defp get_offset(headers) do
+    case List.keyfind(headers, "range", 0) do
+      {"range", "bytes=" <> start_pos} ->
+        String.split(start_pos, "-") |> hd |> String.to_integer
+      nil ->
+        0
     end
+  end
   
-    defp get_file_size(path) do
-      {:ok, %{size: size}} = File.stat path
+  defp get_file_size(path) do
+    {:ok, %{size: size}} = File.stat path
+
+    size
+  end
   
-      size
-    end
-  
-    defp send_video(conn, headers, video) do
-      video_path = video
-      offset = get_offset(headers)
-      file_size = get_file_size(video_path)
-  
-      conn
-      |> Plug.Conn.put_resp_header("content-type", "video/mp4")
-      |> Plug.Conn.put_resp_header("content-range", "bytes #{offset}-#{file_size-1}/#{file_size}")
-      |> Plug.Conn.send_file(206, video_path, offset, file_size - offset)
-    end
+  defp send_video(conn, headers, path) do
+    offset = get_offset(headers)
+    file_size = get_file_size(path)
+
+    conn
+    |> Plug.Conn.put_resp_header("content-type", "video/mp4")
+    |> Plug.Conn.put_resp_header("content-range", "bytes #{offset}-#{file_size-1}/#{file_size}")
+    |> Plug.Conn.send_file(206, path, offset, file_size - offset)
+  end
 end
