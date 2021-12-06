538e3976200eee97ea7921edb10bea0dff92dbe8
trueChazza <cgnetwork.nz@gmail.com>
2021-12-06 16:08:30 +1300

    fix(common): fix watcher shows dir

--- lib/media_server/application.ex
+++ lib/media_server/application.ex
@@ -18,7 +18,7 @@ defmodule MediaServer.Application do
       MediaServerWeb.Endpoint,
       # Start a worker by calling: MediaServer.Worker.start_link(arg)
       # {MediaServer.Worker, arg}
-      {MediaServer.Media.Watcher, dirs: ["/movies", "shows"]},
+      {MediaServer.Media.Watcher, dirs: ["/movies", "/shows"]},
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
