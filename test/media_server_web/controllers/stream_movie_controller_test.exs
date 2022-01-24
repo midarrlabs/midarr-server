ff8968b30d9794b4884e9c280795cd3f4af7db62
trueChazza <cgnetwork.nz@gmail.com>
2022-01-24 16:48:42 +1300

    test(common): add stream range cases

--- test/media_server_web/controllers/stream_movie_controller_test.exs
+++ test/media_server_web/controllers/stream_movie_controller_test.exs
@@ -32,5 +32,26 @@ defmodule MediaServerWeb.StreamMovieControllerTest do
       assert Enum.member?(conn.resp_headers, {"content-type", "video/mp4"})
       assert Enum.member?(conn.resp_headers, {"content-range", "bytes 0-133/134"})
     end
+
+    test "movie range", %{conn: conn, user: user} do
+
+      {_radarr, movie_id} = real_radarr_fixture()
+
+      conn =
+        post(conn, Routes.user_session_path(conn, :create), %{
+          "user" => %{"email" => user.email, "password" => valid_user_password()}
+        })
+
+      assert get_session(conn, :user_token)
+      assert redirected_to(conn) == "/"
+
+      conn = conn |> recycle() |> put_req_header("range", "bytes=124-")
+      conn = get(conn, "/movies/#{ movie_id }/stream")
+
+      assert conn.status === 206
+      assert conn.state === :file
+      assert Enum.member?(conn.resp_headers, {"content-type", "video/mp4"})
+      assert Enum.member?(conn.resp_headers, {"content-range", "bytes 124-133/134"})
+    end
   end
 end
