99364b920c118ed63117c01d133c9a1eb3c341be
trueChazza <cgnetwork.nz@gmail.com>
2022-01-28 16:47:23 +1300

    refactor(common): remove settings mount

--- lib/media_server_web/live/settings_live/index.ex
+++ lib/media_server_web/live/settings_live/index.ex
@@ -6,24 +6,16 @@ defmodule MediaServerWeb.SettingsLive.Index do
   alias MediaServer.Integrations
 
   @impl true
-  def mount(_params, _session, socket) do
-    {
-      :ok,
-      socket
-      |> assign(:page_title, "Settings")
-      |> assign(:users, Repo.all(User))
-      |> assign(:user, User.registration_changeset(%User{}, %{}))
-      |> assign(:radarr, Integrations.get_first_radarr())
-      |> assign(:sonarr, Integrations.get_first_sonarr())
-    }
-  end
-
-  @impl true
   def handle_params(params, _url, socket) do
     {:noreply, apply_action(socket, socket.assigns.live_action, params)}
   end
 
   defp apply_action(socket, :index, _params) do
     socket
+    |> assign(:page_title, "Settings")
+    |> assign(:users, Repo.all(User))
+    |> assign(:user, User.registration_changeset(%User{}, %{}))
+    |> assign(:radarr, Integrations.get_first_radarr())
+    |> assign(:sonarr, Integrations.get_first_sonarr())
   end
 end
