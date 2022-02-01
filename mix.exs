bcab50aba8b388924786efcd90c5aa25c4bc87f1
trueChazza <cgnetwork.nz@gmail.com>
2022-02-01 15:05:50 +1300

    build(core): upgrade phoenix_live_view 0.17

--- mix.exs
+++ mix.exs
@@ -40,7 +40,7 @@ defmodule MediaServer.MixProject do
       {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 3.0"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:phoenix_live_view, "~> 0.16.0"},
+      {:phoenix_live_view, "~> 0.17", override: true},
       {:floki, ">= 0.30.0", only: :test},
       {:phoenix_live_dashboard, "~> 0.5"},
       {:esbuild, "~> 0.2", runtime: Mix.env() == :dev},
