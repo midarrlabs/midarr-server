d91bd9543f545ff747165cf0ed306aa67374f8da
trueChazza <cgnetwork.nz@gmail.com>
2022-02-02 15:10:32 +1300

    feat(common): add home hero

--- lib/media_server_web/live/home_live/index.ex
+++ lib/media_server_web/live/home_live/index.ex
@@ -7,9 +7,24 @@ defmodule MediaServerWeb.HomeLive.Index do
   end
 
   defp apply_action(socket, :index, _params) do
-    socket
-    |> assign(page_title: "Home")
-    |> assign(:latest_movies, MediaServerWeb.Repositories.Movies.get_latest(6))
-    |> assign(:latest_series, MediaServerWeb.Repositories.Series.get_latest(6))
+
+    latest_movies = MediaServerWeb.Repositories.Movies.get_latest(7)
+
+    if !Enum.empty?(latest_movies) do
+      {:ok, latest_movie} = latest_movies |> Enum.fetch(0)
+
+      socket
+      |> assign(page_title: "Home")
+      |> assign(:latest_movie, latest_movie)
+      |> assign(:latest_movies, latest_movies |> Enum.drop(1))
+      |> assign(:latest_series, MediaServerWeb.Repositories.Series.get_latest(6))
+
+    else
+      socket
+      |> assign(page_title: "Home")
+      |> assign(:latest_movie, %{})
+      |> assign(:latest_movies, MediaServerWeb.Repositories.Movies.get_latest(6))
+      |> assign(:latest_series, MediaServerWeb.Repositories.Series.get_latest(6))
+    end
   end
 end
