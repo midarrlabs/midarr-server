86617c3b77efb7787de1389debd416a982e57e0a
trueChazza <cgnetwork.nz@gmail.com>
2022-02-07 23:11:54 +1300

    fix: fix entry-point.sh

--- entry-point.sh
+++ entry-point.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-mix ecto.migrate
-mix run priv/repo/seeds.exs
+while ! pg_isready -q -h $DB_HOSTNAME -U $DB_USERNAME
+do
+  echo "$(date) - waiting for database to start"
+  sleep 2
+done
+
+if [[ -z `psql -Atqc "\\list $DB_DATABASE"` ]]; then
+  mix ecto.migrate
+  mix run priv/repo/seeds.exs
+  echo "Database $DB_DATABASE ready"
+fi
+
 exec mix phx.server
