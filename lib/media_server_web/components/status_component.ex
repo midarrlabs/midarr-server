82b74b339c3f84923f3238277e2f65113082417b
trueChazza <cgnetwork.nz@gmail.com>
2022-02-22 20:30:45 +1300

    feat: add delete continue watching movie

--- lib/media_server_web/components/status_component.ex
+++ lib/media_server_web/components/status_component.ex
@@ -1,4 +1,12 @@
 defmodule MediaServerWeb.Components.StatusComponent do
   use MediaServerWeb, :live_component
 
+  alias MediaServer.WatchStatuses
+
+  @impl true
+  def handle_event("delete", %{"id" => id}, socket) do
+    WatchStatuses.delete_movie(WatchStatuses.get_movie!(id))
+
+    {:noreply, socket}
+  end
 end
