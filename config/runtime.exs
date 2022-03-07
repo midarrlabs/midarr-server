c3945d3062051c5908be02b2690c09279468f8b8
trueChazza <cgnetwork.nz@gmail.com>
2022-03-08 00:13:14 +1300

    fix: disable check origin

--- config/runtime.exs
+++ config/runtime.exs
@@ -29,6 +29,7 @@ if config_env() == :prod do
       ip: {0, 0, 0, 0, 0, 0, 0, 0},
       port: String.to_integer(System.get_env("PORT") || "4000")
     ],
+    check_origin: false,
     secret_key_base: secret_key_base
 
   # ## Using releases
