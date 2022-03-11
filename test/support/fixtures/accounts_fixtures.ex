528aa72e526324bc5e0ec37ed73b7a543709da96
trueChazza <cgnetwork.nz@gmail.com>
2022-03-11 21:11:26 +1300

    test: refactor accounts fixtures

--- test/support/fixtures/accounts_fixtures.ex
+++ test/support/fixtures/accounts_fixtures.ex
@@ -44,7 +44,7 @@ defmodule MediaServer.AccountsFixtures do
 
   def extract_user_token(fun) do
     {:ok, captured_email} = fun.(&"[TOKEN]#{&1}[TOKEN]")
-    [_, token | _] = String.split(captured_email.text_body, "[TOKEN]")
+    [_, token | _] = String.split(captured_email.html_body, "[TOKEN]")
     token
   end
 end
