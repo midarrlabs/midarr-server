402d50e1cab76fc42a05c6e0880dd6f59b22ed7c
trueChazza <cgnetwork.nz@gmail.com>
2022-03-29 11:50:31 +1300

    test: refactor browser test

--- test/media_server_web/browser_test.exs
+++ test/media_server_web/browser_test.exs
@@ -1,10 +1,18 @@
 defmodule MediaServerWeb.BrowserTest do
-  use MediaServerWeb.ConnCase
+  use ExUnit.Case
   use Wallaby.Feature
 
   feature "test", %{session: session} do
+
     session
     |> visit("/")
     |> assert_has(Query.text("Login to your account"))
+    |> find(Query.css(".space-y-6"))
+    |> fill_in(Query.text_field("user[email]"), with: "admin@email.com")
+    |> fill_in(Query.text_field("user[password]"), with: "passwordpassword")
+    |> click(Query.button("Login"))
+
+#    timeout
+#    session take screenshot
   end
 end
