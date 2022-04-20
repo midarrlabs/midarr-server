3d633fdc022fc0138561e5b60ec037969f5f6375
trueChazza <cgnetwork.nz@gmail.com>
2022-04-20 18:27:30 +1200

    Revert "fix: add tailwind to assets deploy"
    This reverts commit b46d13a3bd39f070f213ce4c5558370e2502e3be.

--- mix.exs
+++ mix.exs
@@ -71,7 +71,7 @@ defmodule MediaServer.MixProject do
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
       test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
-      "assets.deploy": ["tailwind default --minify", "esbuild default --minify", "phx.digest"]
+      "assets.deploy": ["esbuild default --minify", "phx.digest"]
     ]
   end
 end
