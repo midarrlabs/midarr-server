6590f88f2586e45b7119c73a90a056e9c323481d
trueChazza <cgnetwork.nz@gmail.com>
2022-04-20 21:56:15 +1200

    refactor: refactor movies

--- lib/media_server/repositories/movies.ex
+++ lib/media_server/repositories/movies.ex
@@ -10,29 +10,4 @@ defmodule MediaServer.Repositories.Movies do
   def get_all(pid) do
     Agent.get(pid, fn state -> state end)
   end
-
-  def get_movie(pid, id) do
-    Agent.get(pid, fn state ->
-      Enum.filter(state, fn item -> item["id"] === id end)
-      |> List.first()
-    end)
-  end
-
-  def get_latest(pid, amount) do
-    Agent.get(pid, fn state ->
-      Enum.filter(state, fn item -> item["hasFile"] end)
-      |> Enum.sort_by(& &1["movieFile"]["dateAdded"], :desc)
-      |> Enum.take(amount)
-    end)
-  end
-
-  def get_path(pid, id) do
-    movie =
-      Agent.get(pid, fn state ->
-        Enum.filter(state, fn item -> item["id"] === id end)
-        |> List.first()
-      end)
-
-    movie["movieFile"]["path"]
-  end
 end
--- test/media_server/repositories/movies_test.exs
+++ test/media_server/repositories/movies_test.exs
@@ -14,28 +14,4 @@ defmodule MediaServer.Repositories.MoviesTest do
 
     assert MediaServer.Repositories.Movies.get_all(pid) === movie
   end
-
-  test "get movie", %{pid: pid} do
-    fixture = MoviesFixtures.get_movie()
-
-    movie = Movies.get_movie(fixture["id"])
-
-    assert MediaServer.Repositories.Movies.get_movie(pid, fixture["id"]) === movie
-  end
-
-  test "get latest", %{pid: pid} do
-    movie = Movies.get_all()
-
-    assert MediaServer.Repositories.Movies.get_latest(pid, 2) ===
-             Enum.reject(movie, fn item -> item["id"] === 1 end)
-  end
-
-  test "get path", %{pid: pid} do
-    fixture = MoviesFixtures.get_movie()
-
-    movie = Movies.get_movie(fixture["id"])
-
-    assert MediaServer.Repositories.Movies.get_path(pid, fixture["id"]) ===
-             movie["movieFile"]["path"]
-  end
 end
