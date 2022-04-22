50726d14b71c2d105a6b1aa138d3ae6485de385a
trueChazza <cgnetwork.nz@gmail.com>
2022-04-22 16:25:13 +1200

    refactor: change pagination function component

--- lib/media_server_web/components/pagination_component.ex
+++ lib/media_server_web/components/pagination_component.ex
@@ -1,3 +1,25 @@
 defmodule MediaServerWeb.Components.PaginationComponent do
-  use MediaServerWeb, :live_component
+  use Phoenix.Component
+
+  def render(assigns) do
+    ~H"""
+    <nav class="bg-white px-4 py-3 flex items-center justify-between border-t border-gray-200 sm:px-6">
+
+      <div class="hidden sm:block">
+        <p class="text-sm text-gray-700">
+          Showing
+          <span class="font-medium"><%= assigns.page_number %></span>
+          of
+          <span class="font-medium"><%= assigns.total_pages %></span>
+          pages
+        </p>
+      </div>
+
+      <div class="flex-1 flex justify-between sm:justify-end">
+        <a href="#" class="relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"> Previous </a>
+        <a href="#" class="ml-3 relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"> Next </a>
+      </div>
+    </nav>
+    """
+  end
 end
--- test/media_server_web/components/pagination_component_test.exs
+++ test/media_server_web/components/pagination_component_test.exs
@@ -5,7 +5,7 @@ defmodule MediaServerWeb.PaginationComponentTest do
 
   test "it should" do
 
-    assert render_component(MediaServerWeb.Components.PaginationComponent, page_number: 1, total_pages: 10) =~ "1</span>\n      of"
-    assert render_component(MediaServerWeb.Components.PaginationComponent, page_number: 1, total_pages: 10) =~ "10</span>\n      pages"
+    assert render_component(&MediaServerWeb.Components.PaginationComponent.render/1, page_number: 1, total_pages: 10) =~ "1</span>\n      of"
+    assert render_component(&MediaServerWeb.Components.PaginationComponent.render/1, page_number: 1, total_pages: 10) =~ "10</span>\n      pages"
   end
 end
