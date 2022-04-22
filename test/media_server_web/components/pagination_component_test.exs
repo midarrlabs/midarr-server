80be6771bf88fc3f566d80ee19442b9d8319f4c3
trueChazza <cgnetwork.nz@gmail.com>
2022-04-22 16:18:02 +1200

    feat: add pagination component page meta

--- test/media_server_web/components/pagination_component_test.exs
+++ test/media_server_web/components/pagination_component_test.exs
@@ -5,6 +5,7 @@ defmodule MediaServerWeb.PaginationComponentTest do
 
   test "it should" do
 
-    assert render_component(MediaServerWeb.Components.PaginationComponent) =~ "Previous"
+    assert render_component(MediaServerWeb.Components.PaginationComponent, page_number: 1, total_pages: 10) =~ "1</span>\n      of"
+    assert render_component(MediaServerWeb.Components.PaginationComponent, page_number: 1, total_pages: 10) =~ "10</span>\n      pages"
   end
 end
