3bfe7fd5240d8d67a390287110bd93728da23768
Chaz Goodare <cgnetwork.nz@gmail.com>
2022-04-22 18:11:03 +1200

    feat: add movies pagination

--- lib/media_server_web/live/movies_live/index.ex
+++ lib/media_server_web/live/movies_live/index.ex
@@ -4,12 +4,21 @@ defmodule MediaServerWeb.MoviesLive.Index do
   alias MediaServerWeb.Repositories.Movies
 
   @impl true
+  def handle_params(%{"page" => page}, _url, socket) do
+    {
+      :noreply,
+      socket
+      |> assign(:page_title, "Movies")
+      |> assign(:movies, Scrivener.paginate(Movies.get_all(), %{"page" => page, "page_size" => "50"}))
+    }
+  end
+
   def handle_params(_params, _url, socket) do
     {
       :noreply,
       socket
       |> assign(:page_title, "Movies")
-      |> assign(:movies, Movies.get_all())
+      |> assign(:movies, Scrivener.paginate(Movies.get_all(), %{"page" => "1", "page_size" => "50"}))
     }
   end
 end
