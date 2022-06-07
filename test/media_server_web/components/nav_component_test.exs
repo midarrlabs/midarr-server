21cf03d889be091f33dcbcedd9321f6608449c1a
trueChazza <cgnetwork.nz@gmail.com>
2022-06-07 17:17:00 +1200

    test: add initial nav_component_test

---
+++ test/media_server_web/components/nav_component_test.exs
@@ -0,0 +1,9 @@
+defmodule MediaServerWeb.Components.NavComponentTest do
+  use MediaServerWeb.ConnCase
+
+  import Phoenix.LiveViewTest
+
+  test "it should", %{conn: conn} do
+    assert true
+  end
+end
