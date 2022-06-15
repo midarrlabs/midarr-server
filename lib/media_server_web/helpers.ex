424804f92909be62679da31e56fd25cf58f4714f
trueChazza <cgnetwork.nz@gmail.com>
2022-06-15 17:03:22 +1200

    refactor: move send video to helper from controllers

--- lib/media_server_web/helpers.ex
+++ lib/media_server_web/helpers.ex
@@ -1,4 +1,43 @@
 defmodule MediaServerWeb.Helpers do
+
+  import Plug.Conn
+
+  def get_offset(headers) do
+    case List.keyfind(headers, "range", 0) do
+      {"range", "bytes=" <> start_pos} ->
+        String.split(start_pos, "-") |> hd |> String.to_integer()
+
+      nil ->
+        0
+    end
+  end
+
+  def get_file_size(path) do
+    {:ok, %{size: size}} = File.stat(path)
+
+    size
+  end
+
+  def send_video(conn, headers, path) do
+    file_size = get_file_size(path)
+
+    case List.keyfind(headers, "range", 0) do
+      {"range", "bytes=0-1"} ->
+        conn
+        |> put_resp_header("content-type", "video/mp4")
+        |> put_resp_header("content-range", "bytes 0-1/#{file_size}")
+        |> send_file(206, path, 0, 2)
+
+      _ ->
+        offset = get_offset(headers)
+
+        conn
+        |> put_resp_header("content-type", "video/mp4")
+        |> put_resp_header("content-range", "bytes #{offset}-#{file_size - 1}/#{file_size}")
+        |> send_file(206, path, offset, file_size - offset)
+    end
+  end
+
   def minutes_remaining_from_seconds(duration, current_time) do
     ceil((duration - current_time) / 60)
   end
