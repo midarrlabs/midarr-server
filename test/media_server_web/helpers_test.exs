0923f87a2578f61ee3c513947add1b0f2463c81c
trueChazza <cgnetwork.nz@gmail.com>
2022-06-25 16:28:20 +1200

    test: remove helpers_test file read expectations

--- test/media_server_web/helpers_test.exs
+++ test/media_server_web/helpers_test.exs
@@ -10,35 +10,25 @@ defmodule MediaServerWeb.HelpersTest do
   describe "movie subtitles" do
     @path "fixtures/movies/Caminandes Llama Drama (2013)"
     @file_name "Caminandes.Llama.Drama.1080p.mp4"
-    @expected ["Caminandes.Llama.Drama.1080p.en.srt", "Caminandes.Llama.Drama.1080p.mp4"]
     @filtered "Caminandes.Llama.Drama.1080p.en.srt"
 
     test "it gets subtitle" do
-      assert File.ls!(@path) == @expected
-
       assert MediaServerWeb.Helpers.get_subtitle(@path, @file_name) == @filtered
     end
 
     test "it has subtitle" do
-      assert File.ls!(@path) == @expected
-
       assert MediaServerWeb.Helpers.has_subtitle(@path, @file_name)
     end
 
     @path_without "fixtures/movies/Caminandes Gran Dillama (2013)"
     @file_name_without "Caminandes.Gran.Dillama.1080p.mp4"
-    @expected_without ["Caminandes.Gran.Dillama.1080p.mp4"]
     @filtered_without nil
 
     test "it does not get subtitle" do
-      assert File.ls!(@path_without) == @expected_without
-
       assert MediaServerWeb.Helpers.get_subtitle(@path_without, @file_name_without) == @filtered_without
     end
 
     test "it does not have subtitle" do
-      assert File.ls!(@path_without) == @expected_without
-
       refute MediaServerWeb.Helpers.has_subtitle(@path_without, @file_name_without)
     end
   end
@@ -46,23 +36,13 @@ defmodule MediaServerWeb.HelpersTest do
   describe "episode subtitles" do
     @path "fixtures/shows/Pioneer One/Season 1"
     @file_name "Pioneer.One.S01E02.The.Man.From.Mars.720p.mp4"
-    @expected [
-      "Pioneer.One.S01E01.Earthfall.720p.en.srt",
-      "Pioneer.One.S01E01.Earthfall.720p.mp4",
-      "Pioneer.One.S01E02.The.Man.From.Mars.720p.en.srt",
-      "Pioneer.One.S01E02.The.Man.From.Mars.720p.mp4"
-    ]
     @filtered "Pioneer.One.S01E02.The.Man.From.Mars.720p.en.srt"
 
     test "it gets subtitle" do
-      assert File.ls!(@path) == @expected
-
       assert MediaServerWeb.Helpers.get_subtitle(@path, @file_name) == @filtered
     end
 
     test "it has subtitle" do
-      assert File.ls!(@path) == @expected
-
       assert MediaServerWeb.Helpers.has_subtitle(@path, @file_name)
     end
   end
