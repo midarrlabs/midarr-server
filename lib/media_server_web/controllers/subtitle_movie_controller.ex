fbf90b52f6752a93b84b51f17e16aa3610a5b043
trueChazza <cgnetwork.nz@gmail.com>
2022-06-28 17:26:39 +1200

    refactor: minor refactor

--- lib/media_server_web/controllers/subtitle_movie_controller.ex
+++ lib/media_server_web/controllers/subtitle_movie_controller.ex
@@ -4,7 +4,7 @@ defmodule MediaServerWeb.SubtitleMovieController do
   alias MediaServerWeb.Repositories.Movies
   alias MediaServer.Extitle
 
-  def show(%{req_headers: headers} = conn, %{"id" => id}) do
+  def show(conn, %{"id" => id}) do
     conn
     |> send_resp(200, Extitle.format(Extitle.parse(Movies.get_subtitle_path_for(id))))
   end
--- lib/media_server_web/extitle.ex
+++ lib/media_server_web/extitle.ex
@@ -1,5 +1,5 @@
 defmodule MediaServer.Extitle do
-  def make_cue([_, timing | lines]) do
+  defp make_cue([_, timing | lines]) do
     [from, to] = timing |> String.split(" --> ")
 
     [
@@ -23,25 +23,18 @@ defmodule MediaServer.Extitle do
 
   defp pad(time, count \\ 2), do: "#{time}" |> String.pad_leading(count, ["0"])
 
-  defp format_microsecond(us), do: "#{div(us, 1000)}" |> pad(3)
-
-  def format_time(%Time{hour: h, minute: m, second: s, microsecond: {us, _}}, sep) do
-    "#{pad(h)}:#{pad(m)}:#{pad(s)}#{sep}#{format_microsecond(us)}"
+  defp format_time(
+         %Time{hour: hour, minute: minute, second: second, microsecond: {us, _}},
+         separator
+       ) do
+    "#{pad(hour)}:#{pad(minute)}:#{pad(second)}#{separator}#{"#{div(us, 1000)}" |> pad(3)}"
   end
 
-  defp build_cue_header(%{from: from, to: to}) do
-    "#{format_time(from, ".")} --> #{format_time(to, ".")}\n"
-  end
-
-  defp build_cue_text(parts), do: parts |> Enum.map(& &1) |> Enum.join("\n")
-
-  defp build(sub) do
-    build_cue_header(sub) <> build_cue_text(sub.text)
-  end
-
-  defp build_cues(sub) do
-    sub
-    |> Enum.map(&build(&1))
+  defp build_cues(cues) do
+    cues
+    |> Enum.map(fn %{from: from, to: to, text: text} ->
+      "#{format_time(from, ".")} --> #{format_time(to, ".")}\n" <> Enum.join(text, "\n")
+    end)
     |> Enum.join("\n\n")
   end
 
