6fe0509f396a1ff3a331d1ca24387a7e28761b44
trueChazza <cgnetwork.nz@gmail.com>
2022-07-13 17:25:34 +1200

    refactor: refactor movies page

--- lib/media_server_web/live/movies_live/index.ex
+++ lib/media_server_web/live/movies_live/index.ex
@@ -13,25 +13,40 @@ defmodule MediaServerWeb.MoviesLive.Index do
   end
 
   @impl true
+  def handle_params(_params, _url, socket) do
+    pid = self()
+
+    Task.start(fn ->
+      send(pid,{:default, %{"movies" => Movies.get_all()}})
+    end)
+
+    {:noreply, socket}
+  end
+
   def handle_params(%{"page" => page}, _url, socket) do
+    pid = self()
+
+    Task.start(fn ->
+      send(pid,{:paged, %{"movies" => Movies.get_all(), "page" => page}})
+    end)
+
+    {:noreply, socket}
+  end
+
+  @impl true
+  def handle_info({:default, %{"movies" => movies}}, socket) do
     {
       :noreply,
       socket
-      |> assign(
-        :movies,
-        Scrivener.paginate(Movies.get_all(), %{"page" => page, "page_size" => "50"})
-      )
+      |> assign(:movies, Scrivener.paginate(movies, %{"page" => "1", "page_size" => "50"}))
     }
   end
 
-  def handle_params(_params, _url, socket) do
+  def handle_info({:paged, %{"movies" => movies, "page" => page}}, socket) do
     {
       :noreply,
       socket
-      |> assign(
-        :movies,
-        Scrivener.paginate(Movies.get_all(), %{"page" => "1", "page_size" => "50"})
-      )
+      |> assign(:movies, Scrivener.paginate(movies, %{"page" => page, "page_size" => "50"}))
     }
   end
 end
