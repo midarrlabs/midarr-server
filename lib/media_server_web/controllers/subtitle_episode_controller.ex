a373e0718f1f8c6581ccfbe023faed5b02741639
trueChazza <cgnetwork.nz@gmail.com>
2022-07-19 10:07:49 +1200

    refactor: refactor extitles

--- lib/media_server_web/controllers/subtitle_episode_controller.ex
+++ lib/media_server_web/controllers/subtitle_episode_controller.ex
@@ -2,10 +2,9 @@ defmodule MediaServerWeb.SubtitleEpisodeController do
   use MediaServerWeb, :controller
 
   alias MediaServerWeb.Repositories.Episodes
-  alias MediaServer.Extitle
 
   def show(conn, %{"id" => id}) do
     conn
-    |> send_resp(200, Extitle.format(Extitle.parse(Episodes.get_subtitle_path_for(id))))
+    |> send_resp(200, Extitles.format(Extitles.parse(Episodes.get_subtitle_path_for(id))))
   end
 end
--- lib/media_server_web/controllers/subtitle_movie_controller.ex
+++ lib/media_server_web/controllers/subtitle_movie_controller.ex
@@ -2,10 +2,9 @@ defmodule MediaServerWeb.SubtitleMovieController do
   use MediaServerWeb, :controller
 
   alias MediaServerWeb.Repositories.Movies
-  alias MediaServer.Extitle
 
   def show(conn, %{"id" => id}) do
     conn
-    |> send_resp(200, Extitle.format(Extitle.parse(Movies.get_subtitle_path_for(id))))
+    |> send_resp(200, Extitles.format(Extitles.parse(Movies.get_subtitle_path_for(id))))
   end
 end
