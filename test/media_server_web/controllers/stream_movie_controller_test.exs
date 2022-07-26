8861573e52dd1b977c18038185226a9508a385c7
trueChazza <cgnetwork.nz@gmail.com>
2022-07-26 15:46:28 +1200

    test: refactor stream_movie_controller_test

--- test/media_server_web/controllers/stream_movie_controller_test.exs
+++ test/media_server_web/controllers/stream_movie_controller_test.exs
@@ -4,114 +4,110 @@ defmodule MediaServerWeb.StreamMovieControllerTest do
   alias MediaServer.AccountsFixtures
   alias MediaServer.MoviesFixtures
 
-  defp create_fixtures(_) do
-    %{user: AccountsFixtures.user_fixture()}
+  setup %{conn: conn} do
+    %{conn: conn, user: AccountsFixtures.user_fixture()}
   end
 
-  describe "GET movie stream" do
-    setup [:create_fixtures]
+  test "movie", %{conn: conn, user: user} do
+    conn =
+      post(conn, Routes.user_session_path(conn, :create), %{
+        "user" => %{"email" => user.email, "password" => AccountsFixtures.valid_user_password()}
+      })
 
-    test "movie", %{conn: conn, user: user} do
-      conn =
-        post(conn, Routes.user_session_path(conn, :create), %{
-          "user" => %{"email" => user.email, "password" => AccountsFixtures.valid_user_password()}
-        })
+    assert get_session(conn, :user_token)
+    assert redirected_to(conn) == "/"
 
-      assert get_session(conn, :user_token)
-      assert redirected_to(conn) == "/"
+    movie = MoviesFixtures.get_movie()
 
-      movie = MoviesFixtures.get_movie()
+    token = Phoenix.Token.sign(conn, "user auth", user.id)
 
-      token = Phoenix.Token.sign(conn, "user auth", user.id)
+    conn = get(conn, Routes.stream_movie_path(conn, :show, movie["id"]), token: token)
 
-      conn = get(conn, Routes.stream_movie_path(conn, :show, movie["id"]), token: token)
+    assert conn.status === 206
+    assert conn.state === :file
+    assert Enum.member?(conn.resp_headers, {"content-type", "video/mp4"})
+    assert Enum.member?(conn.resp_headers, {"content-range", "bytes 0-35103579/35103580"})
+  end
 
-      assert conn.status === 206
-      assert conn.state === :file
-      assert Enum.member?(conn.resp_headers, {"content-type", "video/mp4"})
-      assert Enum.member?(conn.resp_headers, {"content-range", "bytes 0-35103579/35103580"})
-    end
+  test "it halts with random token", %{conn: conn, user: user} do
+    conn =
+      post(conn, Routes.user_session_path(conn, :create), %{
+        "user" => %{"email" => user.email, "password" => AccountsFixtures.valid_user_password()}
+      })
 
-    test "it halts with random token", %{conn: conn, user: user} do
-      conn =
-        post(conn, Routes.user_session_path(conn, :create), %{
-          "user" => %{"email" => user.email, "password" => AccountsFixtures.valid_user_password()}
-        })
+    assert get_session(conn, :user_token)
+    assert redirected_to(conn) == "/"
 
-      assert get_session(conn, :user_token)
-      assert redirected_to(conn) == "/"
+    movie = MoviesFixtures.get_movie()
 
-      movie = MoviesFixtures.get_movie()
+    Phoenix.Token.sign(conn, "user auth", user.id)
 
-      Phoenix.Token.sign(conn, "user auth", user.id)
+    conn = get(conn, Routes.stream_movie_path(conn, :show, movie["id"]), token: "someToken")
 
-      conn = get(conn, Routes.stream_movie_path(conn, :show, movie["id"]), token: "someToken")
+    assert conn.status === 403
+    assert conn.halted
+  end
 
-      assert conn.status === 403
-      assert conn.halted
-    end
+  test "it halts without token", %{conn: conn, user: user} do
+    conn =
+      post(conn, Routes.user_session_path(conn, :create), %{
+        "user" => %{"email" => user.email, "password" => AccountsFixtures.valid_user_password()}
+      })
 
-    test "it halts without token", %{conn: conn, user: user} do
-      conn =
-        post(conn, Routes.user_session_path(conn, :create), %{
-          "user" => %{"email" => user.email, "password" => AccountsFixtures.valid_user_password()}
-        })
+    assert get_session(conn, :user_token)
+    assert redirected_to(conn) == "/"
 
-      assert get_session(conn, :user_token)
-      assert redirected_to(conn) == "/"
+    movie = MoviesFixtures.get_movie()
 
-      movie = MoviesFixtures.get_movie()
+    Phoenix.Token.sign(conn, "user auth", user.id)
 
-      Phoenix.Token.sign(conn, "user auth", user.id)
+    conn = get(conn, Routes.stream_movie_path(conn, :show, movie["id"]))
 
-      conn = get(conn, Routes.stream_movie_path(conn, :show, movie["id"]))
+    assert conn.status === 403
+    assert conn.halted
+  end
 
-      assert conn.status === 403
-      assert conn.halted
-    end
+  test "movie range", %{conn: conn, user: user} do
+    conn =
+      post(conn, Routes.user_session_path(conn, :create), %{
+        "user" => %{"email" => user.email, "password" => AccountsFixtures.valid_user_password()}
+      })
 
-    test "movie range", %{conn: conn, user: user} do
-      conn =
-        post(conn, Routes.user_session_path(conn, :create), %{
-          "user" => %{"email" => user.email, "password" => AccountsFixtures.valid_user_password()}
-        })
+    assert get_session(conn, :user_token)
+    assert redirected_to(conn) == "/"
 
-      assert get_session(conn, :user_token)
-      assert redirected_to(conn) == "/"
+    movie = MoviesFixtures.get_movie()
 
-      movie = MoviesFixtures.get_movie()
+    token = Phoenix.Token.sign(conn, "user auth", user.id)
 
-      token = Phoenix.Token.sign(conn, "user auth", user.id)
+    conn = conn |> recycle() |> put_req_header("range", "bytes=124-")
+    conn = get(conn, Routes.stream_movie_path(conn, :show, movie["id"]), token: token)
 
-      conn = conn |> recycle() |> put_req_header("range", "bytes=124-")
-      conn = get(conn, Routes.stream_movie_path(conn, :show, movie["id"]), token: token)
+    assert conn.status === 206
+    assert conn.state === :file
+    assert Enum.member?(conn.resp_headers, {"content-type", "video/mp4"})
+    assert Enum.member?(conn.resp_headers, {"content-range", "bytes 124-35103579/35103580"})
+  end
 
-      assert conn.status === 206
-      assert conn.state === :file
-      assert Enum.member?(conn.resp_headers, {"content-type", "video/mp4"})
-      assert Enum.member?(conn.resp_headers, {"content-range", "bytes 124-35103579/35103580"})
-    end
+  test "safari probe", %{conn: conn, user: user} do
+    conn =
+      post(conn, Routes.user_session_path(conn, :create), %{
+        "user" => %{"email" => user.email, "password" => AccountsFixtures.valid_user_password()}
+      })
 
-    test "safari probe", %{conn: conn, user: user} do
-      conn =
-        post(conn, Routes.user_session_path(conn, :create), %{
-          "user" => %{"email" => user.email, "password" => AccountsFixtures.valid_user_password()}
-        })
+    assert get_session(conn, :user_token)
+    assert redirected_to(conn) == "/"
 
-      assert get_session(conn, :user_token)
-      assert redirected_to(conn) == "/"
+    movie = MoviesFixtures.get_movie()
 
-      movie = MoviesFixtures.get_movie()
+    token = Phoenix.Token.sign(conn, "user auth", user.id)
 
-      token = Phoenix.Token.sign(conn, "user auth", user.id)
+    conn = conn |> recycle() |> put_req_header("range", "bytes=0-1")
+    conn = get(conn, Routes.stream_movie_path(conn, :show, movie["id"]), token: token)
 
-      conn = conn |> recycle() |> put_req_header("range", "bytes=0-1")
-      conn = get(conn, Routes.stream_movie_path(conn, :show, movie["id"]), token: token)
-
-      assert conn.status === 206
-      assert conn.state === :file
-      assert Enum.member?(conn.resp_headers, {"content-type", "video/mp4"})
-      assert Enum.member?(conn.resp_headers, {"content-range", "bytes 0-1/35103580"})
-    end
+    assert conn.status === 206
+    assert conn.state === :file
+    assert Enum.member?(conn.resp_headers, {"content-type", "video/mp4"})
+    assert Enum.member?(conn.resp_headers, {"content-range", "bytes 0-1/35103580"})
   end
 end
