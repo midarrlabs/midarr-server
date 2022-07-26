4d94f9973c73ba2c4b851e542148751406febede
trueChazza <cgnetwork.nz@gmail.com>
2022-07-26 15:46:15 +1200

    test: refactor stream_episode_controller_test

--- test/media_server_web/controllers/stream_episode_controller_test.exs
+++ test/media_server_web/controllers/stream_episode_controller_test.exs
@@ -5,124 +5,120 @@ defmodule MediaServerWeb.StreamEpisodeControllerTest do
   alias MediaServer.SeriesFixtures
   alias MediaServer.EpisodesFixtures
 
-  defp create_fixtures(_) do
-    %{user: AccountsFixtures.user_fixture()}
+  setup %{conn: conn} do
+    %{conn: conn, user: AccountsFixtures.user_fixture()}
   end
 
-  describe "GET episode stream" do
-    setup [:create_fixtures]
+  test "episode", %{conn: conn, user: user} do
+    conn =
+      post(conn, Routes.user_session_path(conn, :create), %{
+        "user" => %{"email" => user.email, "password" => AccountsFixtures.valid_user_password()}
+      })
 
-    test "episode", %{conn: conn, user: user} do
-      conn =
-        post(conn, Routes.user_session_path(conn, :create), %{
-          "user" => %{"email" => user.email, "password" => AccountsFixtures.valid_user_password()}
-        })
+    assert get_session(conn, :user_token)
+    assert redirected_to(conn) == "/"
 
-      assert get_session(conn, :user_token)
-      assert redirected_to(conn) == "/"
+    serie = SeriesFixtures.get_serie()
 
-      serie = SeriesFixtures.get_serie()
+    episode = EpisodesFixtures.get_episode(serie["id"])
 
-      episode = EpisodesFixtures.get_episode(serie["id"])
+    token = Phoenix.Token.sign(conn, "user auth", user.id)
 
-      token = Phoenix.Token.sign(conn, "user auth", user.id)
+    conn = get(conn, Routes.stream_episode_path(conn, :show, episode["id"]), token: token)
 
-      conn = get(conn, Routes.stream_episode_path(conn, :show, episode["id"]), token: token)
+    assert conn.status === 206
+    assert conn.state === :file
+    assert Enum.member?(conn.resp_headers, {"content-type", "video/mp4"})
+    assert Enum.member?(conn.resp_headers, {"content-range", "bytes 0-47552616/47552617"})
+  end
 
-      assert conn.status === 206
-      assert conn.state === :file
-      assert Enum.member?(conn.resp_headers, {"content-type", "video/mp4"})
-      assert Enum.member?(conn.resp_headers, {"content-range", "bytes 0-47552616/47552617"})
-    end
+  test "it halts with random token", %{conn: conn, user: user} do
+    conn =
+      post(conn, Routes.user_session_path(conn, :create), %{
+        "user" => %{"email" => user.email, "password" => AccountsFixtures.valid_user_password()}
+      })
 
-    test "it halts with random token", %{conn: conn, user: user} do
-      conn =
-        post(conn, Routes.user_session_path(conn, :create), %{
-          "user" => %{"email" => user.email, "password" => AccountsFixtures.valid_user_password()}
-        })
+    assert get_session(conn, :user_token)
+    assert redirected_to(conn) == "/"
 
-      assert get_session(conn, :user_token)
-      assert redirected_to(conn) == "/"
+    serie = SeriesFixtures.get_serie()
 
-      serie = SeriesFixtures.get_serie()
+    episode = EpisodesFixtures.get_episode(serie["id"])
 
-      episode = EpisodesFixtures.get_episode(serie["id"])
+    Phoenix.Token.sign(conn, "user auth", user.id)
 
-      Phoenix.Token.sign(conn, "user auth", user.id)
+    conn = get(conn, Routes.stream_episode_path(conn, :show, episode["id"]), token: "someToken")
 
-      conn = get(conn, Routes.stream_episode_path(conn, :show, episode["id"]), token: "someToken")
+    assert conn.status === 403
+    assert conn.halted
+  end
 
-      assert conn.status === 403
-      assert conn.halted
-    end
+  test "it halts without token", %{conn: conn, user: user} do
+    conn =
+      post(conn, Routes.user_session_path(conn, :create), %{
+        "user" => %{"email" => user.email, "password" => AccountsFixtures.valid_user_password()}
+      })
 
-    test "it halts without token", %{conn: conn, user: user} do
-      conn =
-        post(conn, Routes.user_session_path(conn, :create), %{
-          "user" => %{"email" => user.email, "password" => AccountsFixtures.valid_user_password()}
-        })
+    assert get_session(conn, :user_token)
+    assert redirected_to(conn) == "/"
 
-      assert get_session(conn, :user_token)
-      assert redirected_to(conn) == "/"
+    serie = SeriesFixtures.get_serie()
 
-      serie = SeriesFixtures.get_serie()
+    episode = EpisodesFixtures.get_episode(serie["id"])
 
-      episode = EpisodesFixtures.get_episode(serie["id"])
+    Phoenix.Token.sign(conn, "user auth", user.id)
 
-      Phoenix.Token.sign(conn, "user auth", user.id)
+    conn = get(conn, Routes.stream_episode_path(conn, :show, episode["id"]))
 
-      conn = get(conn, Routes.stream_episode_path(conn, :show, episode["id"]))
+    assert conn.status === 403
+    assert conn.halted
+  end
 
-      assert conn.status === 403
-      assert conn.halted
-    end
+  test "episode range", %{conn: conn, user: user} do
+    conn =
+      post(conn, Routes.user_session_path(conn, :create), %{
+        "user" => %{"email" => user.email, "password" => AccountsFixtures.valid_user_password()}
+      })
 
-    test "episode range", %{conn: conn, user: user} do
-      conn =
-        post(conn, Routes.user_session_path(conn, :create), %{
-          "user" => %{"email" => user.email, "password" => AccountsFixtures.valid_user_password()}
-        })
+    assert get_session(conn, :user_token)
+    assert redirected_to(conn) == "/"
 
-      assert get_session(conn, :user_token)
-      assert redirected_to(conn) == "/"
+    serie = SeriesFixtures.get_serie()
 
-      serie = SeriesFixtures.get_serie()
+    episode = EpisodesFixtures.get_episode(serie["id"])
 
-      episode = EpisodesFixtures.get_episode(serie["id"])
+    token = Phoenix.Token.sign(conn, "user auth", user.id)
 
-      token = Phoenix.Token.sign(conn, "user auth", user.id)
+    conn = conn |> recycle() |> put_req_header("range", "bytes=124-")
+    conn = get(conn, Routes.stream_episode_path(conn, :show, episode["id"]), token: token)
 
-      conn = conn |> recycle() |> put_req_header("range", "bytes=124-")
-      conn = get(conn, Routes.stream_episode_path(conn, :show, episode["id"]), token: token)
+    assert conn.status === 206
+    assert conn.state === :file
+    assert Enum.member?(conn.resp_headers, {"content-type", "video/mp4"})
+    assert Enum.member?(conn.resp_headers, {"content-range", "bytes 124-47552616/47552617"})
+  end
 
-      assert conn.status === 206
-      assert conn.state === :file
-      assert Enum.member?(conn.resp_headers, {"content-type", "video/mp4"})
-      assert Enum.member?(conn.resp_headers, {"content-range", "bytes 124-47552616/47552617"})
-    end
+  test "safari probe", %{conn: conn, user: user} do
+    conn =
+      post(conn, Routes.user_session_path(conn, :create), %{
+        "user" => %{"email" => user.email, "password" => AccountsFixtures.valid_user_password()}
+      })
 
-    test "safari probe", %{conn: conn, user: user} do
-      conn =
-        post(conn, Routes.user_session_path(conn, :create), %{
-          "user" => %{"email" => user.email, "password" => AccountsFixtures.valid_user_password()}
-        })
+    assert get_session(conn, :user_token)
+    assert redirected_to(conn) == "/"
 
-      assert get_session(conn, :user_token)
-      assert redirected_to(conn) == "/"
+    serie = SeriesFixtures.get_serie()
 
-      serie = SeriesFixtures.get_serie()
+    episode = EpisodesFixtures.get_episode(serie["id"])
 
-      episode = EpisodesFixtures.get_episode(serie["id"])
+    token = Phoenix.Token.sign(conn, "user auth", user.id)
 
-      token = Phoenix.Token.sign(conn, "user auth", user.id)
+    conn = conn |> recycle() |> put_req_header("range", "bytes=0-1")
+    conn = get(conn, Routes.stream_episode_path(conn, :show, episode["id"]), token: token)
 
-      conn = conn |> recycle() |> put_req_header("range", "bytes=0-1")
-      conn = get(conn, Routes.stream_episode_path(conn, :show, episode["id"]), token: token)
-
-      assert conn.status === 206
-      assert conn.state === :file
-      assert Enum.member?(conn.resp_headers, {"content-type", "video/mp4"})
-      assert Enum.member?(conn.resp_headers, {"content-range", "bytes 0-1/47552617"})
-    end
+    assert conn.status === 206
+    assert conn.state === :file
+    assert Enum.member?(conn.resp_headers, {"content-type", "video/mp4"})
+    assert Enum.member?(conn.resp_headers, {"content-range", "bytes 0-1/47552617"})
   end
 end
