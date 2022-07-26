c62b88bc4f9f2ab2517dda5b40a1eb7b91e82ffd
trueChazza <cgnetwork.nz@gmail.com>
2022-07-26 16:00:05 +1200

    build: remove gettext

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule MediaServer.MixProject do
       version: "1.15.0",
       elixir: "~> 1.13",
       elixirc_paths: elixirc_paths(Mix.env()),
-      compilers: [:gettext] ++ Mix.compilers(),
+      compilers: Mix.compilers(),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
       deps: deps(),
