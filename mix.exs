639a377c4d456e8cbffdd0af21096c624b137017
trueChazza <chaz.goodare@gmail.com>
2022-08-28 00:18:53 +1200

    fix: fix alias assets deploy

--- mix.exs
+++ mix.exs
@@ -73,7 +73,7 @@ defmodule MediaServer.MixProject do
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
       test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
-      "assets.deploy": ["tailwind default --minify, esbuild default --minify", "phx.digest"]
+      "assets.deploy": ["tailwind default --minify", "esbuild default --minify", "phx.digest"]
     ]
   end
 end
