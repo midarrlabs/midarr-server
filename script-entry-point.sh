a77a39d5b68049c8adcba197a619b23a8c23f9e1
trueChazza <chaz.goodare@gmail.com>
2022-10-07 09:51:41 +1300

    build: refactor script-entry-point

--- script-entry-point.sh
+++ script-entry-point.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-while ! pg_isready -q -h $DB_HOSTNAME -U $DB_USERNAME
-do
-  echo "$(date) - waiting for database to start"
-  sleep 2
-done
-
-if [[ -z `psql -Atqc "\\list $DB_DATABASE"` ]]; then
-  mix ecto.migrate
-  mix run priv/repo/seeds.exs
-  echo "Database $DB_DATABASE ready"
-fi
-
-exec mix phx.server
+mix ecto.migrate
+mix run priv/repo/seeds.exs
+mix phx.server
