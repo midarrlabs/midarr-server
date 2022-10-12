09b1d02dc20c26d03d06cbe757a4a87644ed415d
trueChazza <chaz.goodare@gmail.com>
2022-10-12 17:35:15 +1300

    test: fix api_token_test

--- test/media_server/api_token_test.exs
+++ test/media_server/api_token_test.exs
@@ -4,7 +4,7 @@ defmodule MediaServer.Api.TokenTest do
   alias MediaServer.Api.Token
 
   test "it should verify" do
-    {:ok, result} = Phoenix.Token.verify(MediaServerWeb.Endpoint, "auth", Token.get_token())
+    {:ok, result} = Phoenix.Token.verify(MediaServerWeb.Endpoint, "user auth", Token.get_token())
 
     assert result === MediaServer.Api.Token
   end
