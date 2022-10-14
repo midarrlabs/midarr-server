352276dca6dd9bc4e2dc58fe8d44f07a20c27529
trueChazza <chaz.goodare@gmail.com>
2022-10-14 15:37:17 +1300

    refactor: move notification payload to movie repository

--- lib/media_server_web/repositories/movies.ex
+++ lib/media_server_web/repositories/movies.ex
@@ -1,4 +1,6 @@
 defmodule MediaServerWeb.Repositories.Movies do
+  alias MediaServer.Token
+
   def get_url(url) do
     "#{System.get_env("RADARR_BASE_URL")}/api/v3/#{url}?apiKey=#{System.get_env("RADARR_API_KEY")}"
   end
@@ -31,10 +33,57 @@ defmodule MediaServerWeb.Repositories.Movies do
     |> Enum.sort_by(& &1["title"], :asc)
   end
 
-  def set_notification(payload) do
-    HTTPoison.post(get_url("notification"), Jason.encode!(payload), %{
-      "Content-Type" => "application/json"
-    })
+  def set_notification() do
+    HTTPoison.post(
+      get_url("notification"),
+      Jason.encode!(%{
+        "onGrab" => false,
+        "onDownload" => true,
+        "onUpgrade" => false,
+        "onRename" => false,
+        "onMovieDelete" => false,
+        "onMovieFileDelete" => false,
+        "onMovieFileDeleteForUpgrade" => true,
+        "onHealthIssue" => false,
+        "onApplicationUpdate" => false,
+        "supportsOnGrab" => true,
+        "supportsOnDownload" => true,
+        "supportsOnUpgrade" => true,
+        "supportsOnRename" => true,
+        "supportsOnMovieDelete" => true,
+        "supportsOnMovieFileDelete" => true,
+        "supportsOnMovieFileDeleteForUpgrade" => true,
+        "supportsOnHealthIssue" => true,
+        "supportsOnApplicationUpdate" => true,
+        "includeHealthWarnings" => false,
+        "name" => "Midarr",
+        "fields" => [
+          %{
+            "name" => "url",
+            "value" =>
+              "#{System.get_env("APP_URL")}/api/webhooks/movie?token=#{Token.get_token()}"
+          },
+          %{
+            "name" => "method",
+            "value" => 1
+          },
+          %{
+            "name" => "username"
+          },
+          %{
+            "name" => "password"
+          }
+        ],
+        "implementationName" => "Webhook",
+        "implementation" => "Webhook",
+        "configContract" => "WebhookSettings",
+        "infoLink" => "https://wiki.servarr.com/radarr/supported#webhook",
+        "tags" => []
+      }),
+      %{
+        "Content-Type" => "application/json"
+      }
+    )
   end
 
   def get_notification do
