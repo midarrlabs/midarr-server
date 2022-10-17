89fe7c5c0506653fc0ccbd05877fa63322546299
trueChazza <chaz.goodare@gmail.com>
2022-10-17 15:53:43 +1300

    test: remove webhooks movie_test

--- test/media_server/webhooks/movie_test.exs
+++
@@ -1,9 +0,0 @@
-defmodule MediaServer.Webhooks.MovieTest do
-  use ExUnit.Case
-
-  alias MediaServerWeb.Repositories.Movies
-
-  test "it should have a notification" do
-    assert Enum.count(Movies.get_notification()) === 1
-  end
-end
