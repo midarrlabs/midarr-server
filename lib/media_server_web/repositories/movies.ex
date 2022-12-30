c68b293a79092049cf67f55a13f9260ee1404ca7
trueChazza <chaz.goodare@gmail.com>
2022-12-30 16:40:02 +1300

    refactor: refactor movies repository

--- lib/media_server_web/repositories/movies.ex
+++ lib/media_server_web/repositories/movies.ex
@@ -1,6 +1,24 @@
 defmodule MediaServerWeb.Repositories.Movies do
-  def get_url(url) do
-    "#{System.get_env("RADARR_BASE_URL")}/api/v3/#{url}?apiKey=#{System.get_env("RADARR_API_KEY")}"
+
+  def get(url) do
+    if String.valid?(System.get_env("RADARR_BASE_URL")) and String.valid?(System.get_env("RADARR_API_KEY")) do
+      HTTPoison.get("#{System.get_env("RADARR_BASE_URL")}/api/v3/#{url}", %{
+        "X-Api-Key" => System.get_env("RADARR_API_KEY")
+      })
+    else
+      nil
+    end
+  end
+
+  def post(url, body) do
+    if String.valid?(System.get_env("RADARR_BASE_URL")) and String.valid?(System.get_env("RADARR_API_KEY")) do
+      HTTPoison.post("#{System.get_env("RADARR_BASE_URL")}/api/v3/#{url}", body, %{
+        "X-Api-Key" => System.get_env("RADARR_API_KEY"),
+        "Content-Type" => "application/json"
+      })
+    else
+      nil
+    end
   end
 
   def handle_response({:ok, %HTTPoison.Response{status_code: 200, body: body}}) do
@@ -12,80 +30,75 @@ defmodule MediaServerWeb.Repositories.Movies do
   end
 
   def get_all() do
-    HTTPoison.get(get_url("movie"))
+    get("movie")
     |> handle_response()
     |> Enum.filter(fn item -> item["hasFile"] end)
     |> Enum.sort_by(& &1["title"], :asc)
   end
 
   def get_cast(id) do
-    HTTPoison.get("#{get_url("credit")}&movieId=#{id}")
+    get("credit?movieId=#{id}")
     |> handle_response()
     |> Stream.filter(fn item -> item["type"] === "cast" end)
   end
 
   def search(query) do
-    HTTPoison.get("#{get_url("movie/lookup")}&term=#{URI.encode(query)}")
+    get("movie/lookup?term=#{URI.encode(query)}")
     |> handle_response()
     |> Stream.filter(fn item -> item["hasFile"] end)
     |> Enum.sort_by(& &1["title"], :asc)
   end
 
   def set_notification() do
-    HTTPoison.post(
-      get_url("notification"),
-      Jason.encode!(%{
-        "onGrab" => false,
-        "onDownload" => true,
-        "onUpgrade" => false,
-        "onRename" => false,
-        "onMovieDelete" => false,
-        "onMovieFileDelete" => false,
-        "onMovieFileDeleteForUpgrade" => true,
-        "onHealthIssue" => false,
-        "onApplicationUpdate" => false,
-        "supportsOnGrab" => true,
-        "supportsOnDownload" => true,
-        "supportsOnUpgrade" => true,
-        "supportsOnRename" => true,
-        "supportsOnMovieDelete" => true,
-        "supportsOnMovieFileDelete" => true,
-        "supportsOnMovieFileDeleteForUpgrade" => true,
-        "supportsOnHealthIssue" => true,
-        "supportsOnApplicationUpdate" => true,
-        "includeHealthWarnings" => false,
-        "name" => "Midarr",
-        "fields" => [
-          %{
-            "name" => "url",
-            "value" =>
-              "#{System.get_env("APP_URL")}/api/webhooks/movie?token=#{MediaServer.Token.get_token()}"
-          },
-          %{
-            "name" => "method",
-            "value" => 1
-          },
-          %{
-            "name" => "username"
-          },
-          %{
-            "name" => "password"
-          }
-        ],
-        "implementationName" => "Webhook",
-        "implementation" => "Webhook",
-        "configContract" => "WebhookSettings",
-        "infoLink" => "https://wiki.servarr.com/radarr/supported#webhook",
-        "tags" => []
-      }),
-      %{
-        "Content-Type" => "application/json"
-      }
+    post("notification", Jason.encode!(%{
+      "onGrab" => false,
+      "onDownload" => true,
+      "onUpgrade" => false,
+      "onRename" => false,
+      "onMovieDelete" => false,
+      "onMovieFileDelete" => false,
+      "onMovieFileDeleteForUpgrade" => true,
+      "onHealthIssue" => false,
+      "onApplicationUpdate" => false,
+      "supportsOnGrab" => true,
+      "supportsOnDownload" => true,
+      "supportsOnUpgrade" => true,
+      "supportsOnRename" => true,
+      "supportsOnMovieDelete" => true,
+      "supportsOnMovieFileDelete" => true,
+      "supportsOnMovieFileDeleteForUpgrade" => true,
+      "supportsOnHealthIssue" => true,
+      "supportsOnApplicationUpdate" => true,
+      "includeHealthWarnings" => false,
+      "name" => "Midarr",
+      "fields" => [
+        %{
+          "name" => "url",
+          "value" =>
+            "#{System.get_env("APP_URL")}/api/webhooks/movie?token=#{MediaServer.Token.get_token()}"
+        },
+        %{
+          "name" => "method",
+          "value" => 1
+        },
+        %{
+          "name" => "username"
+        },
+        %{
+          "name" => "password"
+        }
+      ],
+      "implementationName" => "Webhook",
+      "implementation" => "Webhook",
+      "configContract" => "WebhookSettings",
+      "infoLink" => "https://wiki.servarr.com/radarr/supported#webhook",
+      "tags" => []
+    })
     )
   end
 
   def get_notification do
-    HTTPoison.get(get_url("notification"))
+    get("notification")
     |> handle_response()
   end
 end
