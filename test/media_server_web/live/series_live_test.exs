8689b99a5e0c1b88bd3f12b2837395aeb4c3aec4
trueChazza <chaz.goodare@gmail.com>
2023-02-02 12:38:11 +1300

    fix: reinstate play redirect

--- test/media_server_web/live/series_live_test.exs
+++ test/media_server_web/live/series_live_test.exs
@@ -47,8 +47,6 @@ defmodule MediaServerWeb.SeriesLiveTest do
     send(view.pid, {:episodes, Episodes.get_all(series["id"], "1")})
 
     assert render(view) =~ "Play"
-    assert render(view) =~ "5:24"
-    assert render(view) =~ "5:19"
   end
 
   test "it should replace each episode with episode show response", %{conn: _conn} do
