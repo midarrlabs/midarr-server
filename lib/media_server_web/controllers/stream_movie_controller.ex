0dcd5a42a80513ec5b459adfef79a8144a83d592
trueChazza <chaz.goodare@gmail.com>
2023-02-12 16:34:50 +1300

    refactor: remove alias helpers

--- lib/media_server_web/controllers/stream_movie_controller.ex
+++ lib/media_server_web/controllers/stream_movie_controller.ex
@@ -1,9 +1,7 @@
 defmodule MediaServerWeb.StreamMovieController do
   use MediaServerWeb, :controller
 
-  alias MediaServerWeb.Helpers
-
   def show(%{req_headers: headers} = conn, %{"id" => id}) do
-    Helpers.send_video(conn, headers, MediaServer.MoviesIndex.get_movie_path(id))
+    MediaServerWeb.Helpers.send_video(conn, headers, MediaServer.MoviesIndex.get_movie_path(id))
   end
 end
