9ecd39cdd1b21552b697826dbbcaae7e2c95e4e4
trueChazza <chaz.goodare@gmail.com>
2023-03-01 12:59:35 +1300

    build: prep release

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule MediaServer.MixProject do
   def project do
     [
       app: :media_server,
-      version: "2.1.0",
+      version: "3.0.0",
       elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: Mix.compilers(),
