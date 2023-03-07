fe6815e0bfe681b35031dbcd9c5b96b7a4465030
trueChazza <chaz.goodare@gmail.com>
2023-03-07 17:10:43 +1300

    refactor: refactor layout

--- lib/media_server_web/components/poster_component.ex
+++ lib/media_server_web/components/poster_component.ex
@@ -3,13 +3,17 @@ defmodule MediaServerWeb.Components.PosterComponent do
 
   def render(assigns) do
     ~H"""
-    <div class="bg-blend-overlay relative aspect-auto w-40 md:w-48 flex-none overflow-hidden rounded-sm hover:opacity-75">
+    <div class="bg-blend-overlay relative aspect-auto w-40 md:w-48 h-64 md:h-72 flex-none overflow-hidden rounded-sm hover:opacity-75">
       <%= live_redirect to: assigns.link do %>
-        <img
-          alt={assigns.title}
-          src={MediaServerWeb.Helpers.reduce_size_for_poster_url(assigns.img_src)}
-          loading="lazy"
-        />
+        <%= if assigns.img_src !== "" do %>
+          <img
+            alt={assigns.title}
+            src={MediaServerWeb.Helpers.reduce_size_for_poster_url(assigns.img_src)}
+            loading="lazy"
+          />
+        <% else %>
+          <div class="dark:bg-zinc-800/90 h-full w-full"></div>
+        <% end %>
       <% end %>
     </div>
     """
