4a32585fbacef1701f4cee920143dc4d350f89ed
trueChazza <chaz.goodare@gmail.com>
2023-03-15 10:04:27 +1300

    feat: add proxy series poster

--- lib/media_server_web/controllers/images_controller.ex
+++ lib/media_server_web/controllers/images_controller.ex
@@ -13,4 +13,17 @@ defmodule MediaServerWeb.ImagesController do
     |> put_resp_header("content-type", "image/image")
     |> send_resp(200, body)
   end
+
+  def index(conn, %{"series" => id, "type" => "poster"}) do
+
+    poster_file = MediaServer.SeriesIndex.get_serie(id)
+                  |> MediaServer.SeriesIndex.get_poster()
+                  |> MediaServer.Helpers.get_poster_file()
+
+    {:ok, %HTTPoison.Response{status_code: 200, body: body}} = HTTPoison.get("https://artworks.thetvdb.com/banners/posters/#{ poster_file }")
+
+    conn
+    |> put_resp_header("content-type", "image/image")
+    |> send_resp(200, body)
+  end
 end
