0c3cf8dbd05121d8c94affea12df4386f5a5ba33
trueChazza <chaz.goodare@gmail.com>
2023-03-22 18:00:53 +1300

    Revert "BREAKING CHANGE: set series root path"
    This reverts commit 7203b8d0767e2a0eecf0e81c35561b3dfc6be17c.

--- test/media_server_web/repositories/episodes_test.exs
+++
@@ -1,7 +0,0 @@
-defmodule MediaServer.EpisodesTest do
-  use ExUnit.Case
-  
-  test "it should get episode path" do
-    assert MediaServerWeb.Repositories.Episodes.get_episode_path("1") === "/series/Pioneer One/Season 1/Pioneer.One.S01E01.Earthfall.720p.mp4"
-  end
-end
