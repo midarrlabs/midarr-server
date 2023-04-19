bc80129cbcf198d7ff027b1c658b965c9310b120
trueChazza <chaz.goodare@gmail.com>
2023-04-19 15:51:16 +1200

    build: resolve phoenix v1.7.2 deps

--- lib/media_server_web/router.ex
+++ lib/media_server_web/router.ex
@@ -8,7 +8,7 @@ defmodule MediaServerWeb.Router do
     plug :accepts, ["html"]
     plug :fetch_session
     plug :fetch_live_flash
-    plug :put_root_layout, {MediaServerWeb.LayoutView, :root}
+    plug :put_root_layout, html: {MediaServerWeb.LayoutView, :root}
     plug :protect_from_forgery
     plug :put_secure_browser_headers
     plug :fetch_current_user
