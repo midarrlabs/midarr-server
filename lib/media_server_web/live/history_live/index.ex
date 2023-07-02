3f5fd3b6499351077534c2dff565e2f7b48c5475
trueChazza <chaz.goodare@gmail.com>
2023-07-02 14:53:09 +1200

    refactor: history

--- lib/media_server_web/live/history_live/index.ex
+++ lib/media_server_web/live/history_live/index.ex
@@ -26,7 +26,7 @@ defmodule MediaServerWeb.HistoryLive.Index do
       :noreply,
       socket
       |> assign(:movie_id, MediaServer.MediaTypes.get_movie_id())
-      |> assign(:user_continues, socket.assigns.current_user.media_actions)
+      |> assign(:user_media_actions, socket.assigns.current_user.media_actions)
     }
   end
 end
