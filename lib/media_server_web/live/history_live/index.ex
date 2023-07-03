615edc212796f0b24aba797e1085f2b5a544a3b9
trueChazza <chaz.goodare@gmail.com>
2023-07-04 11:06:34 +1200

    refactor: scope progress to current user

--- lib/media_server_web/live/history_live/index.ex
+++ lib/media_server_web/live/history_live/index.ex
@@ -3,30 +3,26 @@ defmodule MediaServerWeb.HistoryLive.Index do
 
   import Ecto.Query
 
-  alias MediaServer.Repo
-  alias MediaServer.Accounts
-
   @impl true
   def mount(_params, session, socket) do
     {
       :ok,
       socket
       |> assign(page_title: "History")
-      |> assign(
-        :current_user,
-        Accounts.get_user_by_session_token(session["user_token"])
-        |> Repo.preload(media_actions: from(MediaServer.MediaActions, order_by: [desc: :updated_at]))
-         )
+      |> assign(:current_user, MediaServer.Accounts.get_user_by_session_token(session["user_token"]))
     }
   end
 
   @impl true
   def handle_params(_params, _url, socket) do
+    query = from continue in MediaServer.Continues, where: continue.user_id == ^socket.assigns.current_user.id
+
+    current_user = socket.assigns.current_user |> MediaServer.Repo.preload(media_actions: from(MediaServer.MediaActions, order_by: [desc: :updated_at], preload: [continue: ^query]))
     {
       :noreply,
       socket
       |> assign(:movie_id, MediaServer.MediaTypes.get_movie_id())
-      |> assign(:user_media_actions, socket.assigns.current_user.media_actions)
+      |> assign(:user_media_actions, current_user.media_actions)
     }
   end
 end
