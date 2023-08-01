4e1430d1a439ab563ce7ec08a6c0a83d09230f68
trueChazza <chaz.goodare@gmail.com>
2023-08-01 16:47:51 +1200

    test: refactor search

--- test/media_server_web/live/search_live_test.exs
+++ test/media_server_web/live/search_live_test.exs
@@ -13,26 +13,26 @@ defmodule MediaServerWeb.SearchLiveTest do
     {:ok, view, _disconnected_html} =
       live(conn, Routes.search_index_path(conn, :index, query: "Caminandes"))
 
-    assert assert render(view) =~ "Caminandes:  Llamigos"
-    assert assert render(view) =~ "Caminandes: Gran Dillama"
-    assert assert render(view) =~ "Caminandes: Llama Drama"
+    assert render(view) =~ "Caminandes:  Llamigos"
+    assert render(view) =~ "Caminandes: Gran Dillama"
+    assert render(view) =~ "Caminandes: Llama Drama"
 
-    assert assert render(view) =~ Routes.movies_show_path(conn, :show, 3)
-    assert assert render(view) =~ Routes.movies_show_path(conn, :show, 2)
-    assert assert render(view) =~ Routes.movies_show_path(conn, :show, 1)
+    assert render(view) =~ Routes.movies_show_path(conn, :show, 3)
+    assert render(view) =~ Routes.movies_show_path(conn, :show, 2)
+    assert render(view) =~ Routes.movies_show_path(conn, :show, 1)
   end
 
     test "it should normalise movie query", %{conn: conn} do
       {:ok, view, _disconnected_html} =
         live(conn, Routes.search_index_path(conn, :index, query: "camiNandes"))
 
-      assert assert render(view) =~ "Caminandes:  Llamigos"
-      assert assert render(view) =~ "Caminandes: Gran Dillama"
-      assert assert render(view) =~ "Caminandes: Llama Drama"
+      assert render(view) =~ "Caminandes:  Llamigos"
+      assert render(view) =~ "Caminandes: Gran Dillama"
+      assert render(view) =~ "Caminandes: Llama Drama"
 
-      assert assert render(view) =~ Routes.movies_show_path(conn, :show, 3)
-      assert assert render(view) =~ Routes.movies_show_path(conn, :show, 2)
-      assert assert render(view) =~ Routes.movies_show_path(conn, :show, 1)
+      assert render(view) =~ Routes.movies_show_path(conn, :show, 3)
+      assert render(view) =~ Routes.movies_show_path(conn, :show, 2)
+      assert render(view) =~ Routes.movies_show_path(conn, :show, 1)
     end
 
   test "it should search series", %{conn: conn} do
