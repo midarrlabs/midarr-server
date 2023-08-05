c9c8f21b18151958e0154c7c7287c2e197aa402c
trueChazza <chaz.goodare@gmail.com>
2023-08-06 10:52:01 +1200

    refactor: refactor api movies
    add pagination

--- lib/media_server_web/controllers/movies_controller.ex
+++ lib/media_server_web/controllers/movies_controller.ex
@@ -4,64 +4,178 @@ defmodule MediaServerWeb.MoviesController do
   def index(conn, %{"genre" => genre, "page" => page}) do
     capitalized_genre = String.capitalize(genre)
 
-    movies = Scrivener.paginate(MediaServer.MoviesIndex.genre(MediaServer.MoviesIndex.all(), capitalized_genre), %{
-      "page" => page,
-      "page_size" => "50"
-    })
+    movies =
+      Scrivener.paginate(
+        MediaServer.MoviesIndex.genre(MediaServer.MoviesIndex.all(), capitalized_genre),
+        %{
+          "page" => page,
+          "page_size" => "50"
+        }
+      )
 
     conn
-    |> send_resp(200, Jason.encode!(%{"entries" => Enum.map(movies.entries, fn x -> %{"title" => x["title"]} end)}))
+    |> send_resp(
+      200,
+      Jason.encode!(%{
+        "total" => movies.total_entries,
+        "items" =>
+          Enum.map(movies.entries, fn x ->
+            %{
+              "title" => x["title"],
+              "poster" => MediaServer.Helpers.get_poster(x),
+              "background" => MediaServer.Helpers.get_background(x)
+            }
+          end),
+        "prev_page" =>
+          ~p"/api/movies?page=#{MediaServerWeb.Helpers.get_pagination_previous_link(movies.page_number)}",
+        "next_page" =>
+          ~p"/api/movies?page=#{MediaServerWeb.Helpers.get_pagination_next_link(movies.page_number)}"
+      })
+    )
   end
 
   def index(conn, %{"genre" => genre}) do
     capitalized_genre = String.capitalize(genre)
 
-    movies = Scrivener.paginate(MediaServer.MoviesIndex.genre(MediaServer.MoviesIndex.all(), capitalized_genre), %{
-      "page" => "1",
-      "page_size" => "50"
-    })
+    movies =
+      Scrivener.paginate(
+        MediaServer.MoviesIndex.genre(MediaServer.MoviesIndex.all(), capitalized_genre),
+        %{
+          "page" => "1",
+          "page_size" => "50"
+        }
+      )
 
     conn
-    |> send_resp(200, Jason.encode!(%{"entries" => Enum.map(movies.entries, fn x -> %{"title" => x["title"]} end)}))
+    |> send_resp(
+      200,
+      Jason.encode!(%{
+        "total" => movies.total_entries,
+        "items" =>
+          Enum.map(movies.entries, fn x ->
+            %{
+              "title" => x["title"],
+              "poster" => MediaServer.Helpers.get_poster(x),
+              "background" => MediaServer.Helpers.get_background(x)
+            }
+          end),
+        "prev_page" =>
+          ~p"/api/movies?page=#{MediaServerWeb.Helpers.get_pagination_previous_link(movies.page_number)}",
+        "next_page" =>
+          ~p"/api/movies?page=#{MediaServerWeb.Helpers.get_pagination_next_link(movies.page_number)}"
+      })
+    )
   end
 
   def index(conn, %{"sort_by" => "latest", "page" => page}) do
-    movies = Scrivener.paginate(MediaServer.MoviesIndex.latest(MediaServer.MoviesIndex.all()), %{
-      "page" => page,
-      "page_size" => "50"
-    })
+    movies =
+      Scrivener.paginate(MediaServer.MoviesIndex.latest(MediaServer.MoviesIndex.all()), %{
+        "page" => page,
+        "page_size" => "50"
+      })
 
     conn
-    |> send_resp(200, Jason.encode!(%{"entries" => Enum.map(movies.entries, fn x -> %{"title" => x["title"]} end)}))
+    |> send_resp(
+      200,
+      Jason.encode!(%{
+        "total" => movies.total_entries,
+        "items" =>
+          Enum.map(movies.entries, fn x ->
+            %{
+              "title" => x["title"],
+              "poster" => MediaServer.Helpers.get_poster(x),
+              "background" => MediaServer.Helpers.get_background(x)
+            }
+          end),
+        "prev_page" =>
+          ~p"/api/movies?page=#{MediaServerWeb.Helpers.get_pagination_previous_link(movies.page_number)}",
+        "next_page" =>
+          ~p"/api/movies?page=#{MediaServerWeb.Helpers.get_pagination_next_link(movies.page_number)}"
+      })
+    )
   end
 
   def index(conn, %{"sort_by" => "latest"}) do
-    movies = Scrivener.paginate(MediaServer.MoviesIndex.latest(MediaServer.MoviesIndex.all()), %{
-      "page" => "1",
-      "page_size" => "50"
-    })
+    movies =
+      Scrivener.paginate(MediaServer.MoviesIndex.latest(MediaServer.MoviesIndex.all()), %{
+        "page" => "1",
+        "page_size" => "50"
+      })
 
     conn
-    |> send_resp(200, Jason.encode!(%{"entries" => Enum.map(movies.entries, fn x -> %{"title" => x["title"]} end)}))
+    |> send_resp(
+      200,
+      Jason.encode!(%{
+        "total" => movies.total_entries,
+        "items" =>
+          Enum.map(movies.entries, fn x ->
+            %{
+              "title" => x["title"],
+              "poster" => MediaServer.Helpers.get_poster(x),
+              "background" => MediaServer.Helpers.get_background(x)
+            }
+          end),
+        "prev_page" =>
+          ~p"/api/movies?page=#{MediaServerWeb.Helpers.get_pagination_previous_link(movies.page_number)}",
+        "next_page" =>
+          ~p"/api/movies?page=#{MediaServerWeb.Helpers.get_pagination_next_link(movies.page_number)}"
+      })
+    )
   end
 
   def index(conn, %{"page" => page}) do
-    movies = Scrivener.paginate(MediaServer.MoviesIndex.all(), %{
-      "page" => page,
-      "page_size" => "50"
-    })
+    movies =
+      Scrivener.paginate(MediaServer.MoviesIndex.all(), %{
+        "page" => page,
+        "page_size" => "50"
+      })
 
     conn
-    |> send_resp(200, Jason.encode!(%{"entries" => Enum.map(movies.entries, fn x -> %{"title" => x["title"]} end)}))
+    |> send_resp(
+      200,
+      Jason.encode!(%{
+        "total" => movies.total_entries,
+        "items" =>
+          Enum.map(movies.entries, fn x ->
+            %{
+              "title" => x["title"],
+              "poster" => MediaServer.Helpers.get_poster(x),
+              "background" => MediaServer.Helpers.get_background(x)
+            }
+          end),
+        "prev_page" =>
+          ~p"/api/movies?page=#{MediaServerWeb.Helpers.get_pagination_previous_link(movies.page_number)}",
+        "next_page" =>
+          ~p"/api/movies?page=#{MediaServerWeb.Helpers.get_pagination_next_link(movies.page_number)}"
+      })
+    )
   end
 
   def index(conn, _params) do
-    movies = Scrivener.paginate(MediaServer.MoviesIndex.all(), %{
-      "page" => "1",
-      "page_size" => "50"
-    })
+    movies =
+      Scrivener.paginate(MediaServer.MoviesIndex.all(), %{
+        "page" => "1",
+        "page_size" => "50"
+      })
 
     conn
-    |> send_resp(200, Jason.encode!(%{"entries" => Enum.map(movies.entries, fn x -> %{"title" => x["title"]} end)}))
+    |> send_resp(
+      200,
+      Jason.encode!(%{
+        "total" => movies.total_entries,
+        "items" =>
+          Enum.map(movies.entries, fn x ->
+            %{
+              "title" => x["title"],
+              "poster" => MediaServer.Helpers.get_poster(x),
+              "background" => MediaServer.Helpers.get_background(x)
+            }
+          end),
+        "prev_page" =>
+          ~p"/api/movies?page=#{MediaServerWeb.Helpers.get_pagination_previous_link(movies.page_number)}",
+        "next_page" =>
+          ~p"/api/movies?page=#{MediaServerWeb.Helpers.get_pagination_next_link(movies.page_number)}"
+      })
+    )
   end
 end
--- test/media_server_web/controllers/movies_controller_test.exs
+++ test/media_server_web/controllers/movies_controller_test.exs
@@ -11,62 +11,80 @@ defmodule MediaServerWeb.MoviesControllerTest do
     conn = get(conn, ~p"/api/movies?token=#{user.api_token.token}")
 
     assert conn.status === 200
-    assert conn.resp_body === "{\"entries\":[{\"title\":\"Caminandes:  Llamigos\"},{\"title\":\"Caminandes: Gran Dillama\"},{\"title\":\"Caminandes: Llama Drama\"}]}"
+
+    assert conn.resp_body ===
+             "{\"items\":[{\"background\":\"https://image.tmdb.org/t/p/original/tIX2BIhkhoFq1iwtHYzTSbHbe82.jpg\",\"poster\":\"https://image.tmdb.org/t/p/original/753kJbZ5iS7DUomTKX9qF5Cs5NY.jpg\",\"title\":\"Caminandes:  Llamigos\"},{\"background\":\"https://image.tmdb.org/t/p/original/206A3X9EH42kmdQMECq90SbjHCn.jpg\",\"poster\":\"https://image.tmdb.org/t/p/original/7YtEiEzRBVCr8Sbgo1kVsr3OCMk.jpg\",\"title\":\"Caminandes: Gran Dillama\"},{\"background\":\"https://image.tmdb.org/t/p/original/mjkoC8Vo7fSHuqrbVQdI6cNwKA2.jpg\",\"poster\":\"https://image.tmdb.org/t/p/original/66VPke0YSiyfe97aobbcZ55ts56.jpg\",\"title\":\"Caminandes: Llama Drama\"}],\"next_page\":\"/api/movies?page=2\",\"prev_page\":\"/api/movies?page=0\",\"total\":3}"
   end
 
   test "it should have all paged", %{conn: conn, user: user} do
     conn = get(conn, ~p"/api/movies?page=1&token=#{user.api_token.token}")
 
     assert conn.status === 200
-    assert conn.resp_body === "{\"entries\":[{\"title\":\"Caminandes:  Llamigos\"},{\"title\":\"Caminandes: Gran Dillama\"},{\"title\":\"Caminandes: Llama Drama\"}]}"
+
+    assert conn.resp_body ===
+             "{\"items\":[{\"background\":\"https://image.tmdb.org/t/p/original/tIX2BIhkhoFq1iwtHYzTSbHbe82.jpg\",\"poster\":\"https://image.tmdb.org/t/p/original/753kJbZ5iS7DUomTKX9qF5Cs5NY.jpg\",\"title\":\"Caminandes:  Llamigos\"},{\"background\":\"https://image.tmdb.org/t/p/original/206A3X9EH42kmdQMECq90SbjHCn.jpg\",\"poster\":\"https://image.tmdb.org/t/p/original/7YtEiEzRBVCr8Sbgo1kVsr3OCMk.jpg\",\"title\":\"Caminandes: Gran Dillama\"},{\"background\":\"https://image.tmdb.org/t/p/original/mjkoC8Vo7fSHuqrbVQdI6cNwKA2.jpg\",\"poster\":\"https://image.tmdb.org/t/p/original/66VPke0YSiyfe97aobbcZ55ts56.jpg\",\"title\":\"Caminandes: Llama Drama\"}],\"next_page\":\"/api/movies?page=2\",\"prev_page\":\"/api/movies?page=0\",\"total\":3}"
   end
 
   test "it should NOT have all paged", %{conn: conn, user: user} do
     conn = get(conn, ~p"/api/movies?page=2&token=#{user.api_token.token}")
 
     assert conn.status === 200
-    assert conn.resp_body === "{\"entries\":[]}"
+
+    assert conn.resp_body ===
+             "{\"items\":[],\"next_page\":\"/api/movies?page=3\",\"prev_page\":\"/api/movies?page=1\",\"total\":3}"
   end
 
   test "it should have genre", %{conn: conn, user: user} do
     conn = get(conn, ~p"/api/movies?genre=animation&token=#{user.api_token.token}")
 
     assert conn.status === 200
-    assert conn.resp_body === "{\"entries\":[{\"title\":\"Caminandes:  Llamigos\"},{\"title\":\"Caminandes: Gran Dillama\"},{\"title\":\"Caminandes: Llama Drama\"}]}"
+
+    assert conn.resp_body ===
+             "{\"items\":[{\"background\":\"https://image.tmdb.org/t/p/original/tIX2BIhkhoFq1iwtHYzTSbHbe82.jpg\",\"poster\":\"https://image.tmdb.org/t/p/original/753kJbZ5iS7DUomTKX9qF5Cs5NY.jpg\",\"title\":\"Caminandes:  Llamigos\"},{\"background\":\"https://image.tmdb.org/t/p/original/206A3X9EH42kmdQMECq90SbjHCn.jpg\",\"poster\":\"https://image.tmdb.org/t/p/original/7YtEiEzRBVCr8Sbgo1kVsr3OCMk.jpg\",\"title\":\"Caminandes: Gran Dillama\"},{\"background\":\"https://image.tmdb.org/t/p/original/mjkoC8Vo7fSHuqrbVQdI6cNwKA2.jpg\",\"poster\":\"https://image.tmdb.org/t/p/original/66VPke0YSiyfe97aobbcZ55ts56.jpg\",\"title\":\"Caminandes: Llama Drama\"}],\"next_page\":\"/api/movies?page=2\",\"prev_page\":\"/api/movies?page=0\",\"total\":3}"
   end
 
   test "it should NOT have genre", %{conn: conn, user: user} do
     conn = get(conn, ~p"/api/movies?genre=history&token=#{user.api_token.token}")
 
     assert conn.status === 200
-    assert conn.resp_body === "{\"entries\":[]}"
+
+    assert conn.resp_body ===
+             "{\"items\":[],\"next_page\":\"/api/movies?page=2\",\"prev_page\":\"/api/movies?page=0\",\"total\":0}"
   end
 
   test "it should have genre paged", %{conn: conn, user: user} do
     conn = get(conn, ~p"/api/movies?genre=animation&page=1&token=#{user.api_token.token}")
 
     assert conn.status === 200
-    assert conn.resp_body === "{\"entries\":[{\"title\":\"Caminandes:  Llamigos\"},{\"title\":\"Caminandes: Gran Dillama\"},{\"title\":\"Caminandes: Llama Drama\"}]}"
+
+    assert conn.resp_body ===
+             "{\"items\":[{\"background\":\"https://image.tmdb.org/t/p/original/tIX2BIhkhoFq1iwtHYzTSbHbe82.jpg\",\"poster\":\"https://image.tmdb.org/t/p/original/753kJbZ5iS7DUomTKX9qF5Cs5NY.jpg\",\"title\":\"Caminandes:  Llamigos\"},{\"background\":\"https://image.tmdb.org/t/p/original/206A3X9EH42kmdQMECq90SbjHCn.jpg\",\"poster\":\"https://image.tmdb.org/t/p/original/7YtEiEzRBVCr8Sbgo1kVsr3OCMk.jpg\",\"title\":\"Caminandes: Gran Dillama\"},{\"background\":\"https://image.tmdb.org/t/p/original/mjkoC8Vo7fSHuqrbVQdI6cNwKA2.jpg\",\"poster\":\"https://image.tmdb.org/t/p/original/66VPke0YSiyfe97aobbcZ55ts56.jpg\",\"title\":\"Caminandes: Llama Drama\"}],\"next_page\":\"/api/movies?page=2\",\"prev_page\":\"/api/movies?page=0\",\"total\":3}"
   end
 
   test "it should NOT have genre paged", %{conn: conn, user: user} do
     conn = get(conn, ~p"/api/movies?genre=history&page=1&token=#{user.api_token.token}")
 
     assert conn.status === 200
-    assert conn.resp_body === "{\"entries\":[]}"
+
+    assert conn.resp_body ===
+             "{\"items\":[],\"next_page\":\"/api/movies?page=2\",\"prev_page\":\"/api/movies?page=0\",\"total\":0}"
   end
 
   test "it should sort by latest", %{conn: conn, user: user} do
     conn = get(conn, ~p"/api/movies?sort_by=latest&token=#{user.api_token.token}")
 
     assert conn.status === 200
-    assert conn.resp_body === "{\"entries\":[{\"title\":\"Caminandes:  Llamigos\"},{\"title\":\"Caminandes: Gran Dillama\"},{\"title\":\"Caminandes: Llama Drama\"}]}"
+
+    assert conn.resp_body ===
+             "{\"items\":[{\"background\":\"https://image.tmdb.org/t/p/original/tIX2BIhkhoFq1iwtHYzTSbHbe82.jpg\",\"poster\":\"https://image.tmdb.org/t/p/original/753kJbZ5iS7DUomTKX9qF5Cs5NY.jpg\",\"title\":\"Caminandes:  Llamigos\"},{\"background\":\"https://image.tmdb.org/t/p/original/206A3X9EH42kmdQMECq90SbjHCn.jpg\",\"poster\":\"https://image.tmdb.org/t/p/original/7YtEiEzRBVCr8Sbgo1kVsr3OCMk.jpg\",\"title\":\"Caminandes: Gran Dillama\"},{\"background\":\"https://image.tmdb.org/t/p/original/mjkoC8Vo7fSHuqrbVQdI6cNwKA2.jpg\",\"poster\":\"https://image.tmdb.org/t/p/original/66VPke0YSiyfe97aobbcZ55ts56.jpg\",\"title\":\"Caminandes: Llama Drama\"}],\"next_page\":\"/api/movies?page=2\",\"prev_page\":\"/api/movies?page=0\",\"total\":3}"
   end
 
   test "it should sort by latest paged", %{conn: conn, user: user} do
     conn = get(conn, ~p"/api/movies?sort_by=latest&page=1&token=#{user.api_token.token}")
 
     assert conn.status === 200
-    assert conn.resp_body === "{\"entries\":[{\"title\":\"Caminandes:  Llamigos\"},{\"title\":\"Caminandes: Gran Dillama\"},{\"title\":\"Caminandes: Llama Drama\"}]}"
+
+    assert conn.resp_body ===
+             "{\"items\":[{\"background\":\"https://image.tmdb.org/t/p/original/tIX2BIhkhoFq1iwtHYzTSbHbe82.jpg\",\"poster\":\"https://image.tmdb.org/t/p/original/753kJbZ5iS7DUomTKX9qF5Cs5NY.jpg\",\"title\":\"Caminandes:  Llamigos\"},{\"background\":\"https://image.tmdb.org/t/p/original/206A3X9EH42kmdQMECq90SbjHCn.jpg\",\"poster\":\"https://image.tmdb.org/t/p/original/7YtEiEzRBVCr8Sbgo1kVsr3OCMk.jpg\",\"title\":\"Caminandes: Gran Dillama\"},{\"background\":\"https://image.tmdb.org/t/p/original/mjkoC8Vo7fSHuqrbVQdI6cNwKA2.jpg\",\"poster\":\"https://image.tmdb.org/t/p/original/66VPke0YSiyfe97aobbcZ55ts56.jpg\",\"title\":\"Caminandes: Llama Drama\"}],\"next_page\":\"/api/movies?page=2\",\"prev_page\":\"/api/movies?page=0\",\"total\":3}"
   end
 end
