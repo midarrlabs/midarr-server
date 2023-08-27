edfc24ee9063a96842b1180898f40e134783b6f1
trueChazza <chaz.goodare@gmail.com>
2023-08-27 19:37:14 +1200

    build: bump v4.1.0-beta.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule MediaServer.MixProject do
   def project do
     [
       app: :media_server,
-      version: "4.0.0",
+      version: "4.1.0",
       elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
