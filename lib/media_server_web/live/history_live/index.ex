8a46869ac97763cdca15a8bcb02972c3efae9591
trueChazza <chaz.goodare@gmail.com>
2023-08-30 11:08:18 +1200

    feat: add history movies episodes

--- lib/media_server_web/live/history_live/index.ex
+++ lib/media_server_web/live/history_live/index.ex
@@ -9,16 +9,16 @@ defmodule MediaServerWeb.HistoryLive.Index do
       :ok,
       socket
       |> assign(:current_user, MediaServer.Accounts.get_user_by_session_token(session["user_token"]))
-      |> assign(page_title: "History")
     }
   end
 
   @impl true
-  def handle_params(_params, _url, socket) do
+  def handle_params(%{"filter-by" => "episodes"}, _url, socket) do
     query =
       from ma in MediaServer.MediaActions,
            left_join: continue in MediaServer.Continues,
            on: ma.media_id == continue.media_id and ma.media_type_id == continue.media_type_id and continue.user_id == ^socket.assigns.current_user.id,
+           where: ma.media_type_id == ^MediaServer.MediaTypes.get_episode_id(),
            order_by: [desc: ma.updated_at],
            limit: 12,
            preload: [continue: continue]
@@ -28,7 +28,27 @@ defmodule MediaServerWeb.HistoryLive.Index do
     {
       :noreply,
       socket
-      |> assign(:movie_id, MediaServer.MediaTypes.get_movie_id())
+      |> assign(page_title: "History - Episodes")
+      |> assign(:user_media_actions, current_user.media_actions)
+    }
+  end
+
+  def handle_params(_params, _url, socket) do
+    query =
+      from ma in MediaServer.MediaActions,
+           left_join: continue in MediaServer.Continues,
+           on: ma.media_id == continue.media_id and ma.media_type_id == continue.media_type_id and continue.user_id == ^socket.assigns.current_user.id,
+           where: ma.media_type_id == ^MediaServer.MediaTypes.get_movie_id(),
+           order_by: [desc: ma.updated_at],
+           limit: 12,
+           preload: [continue: continue]
+
+    current_user = socket.assigns.current_user |> MediaServer.Repo.preload([media_actions: query])
+
+    {
+      :noreply,
+      socket
+      |> assign(page_title: "History - Movies")
       |> assign(:user_media_actions, current_user.media_actions)
     }
   end
