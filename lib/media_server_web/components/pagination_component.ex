f903f85c2048841753b4ff6163229a7fb56525c2
trueChazza <chaz.goodare@gmail.com>
2023-09-09 22:42:21 +1200

    refactor: refactor layout

--- lib/media_server_web/components/pagination_component.ex
+++ lib/media_server_web/components/pagination_component.ex
@@ -3,29 +3,48 @@ defmodule MediaServerWeb.Components.PaginationComponent do
 
   def render(assigns) do
     ~H"""
-    <div class="flex">
-      <div class="ml-auto flex flex-col items-end gap-3">
-        <a
+    <div class="flex pt-10">
+    <%= if assigns.page_number > 1 do %>
+      <div class="flex flex-col items-start gap-3">
+        <.link
           class="inline-flex gap-0.5 justify-center overflow-hidden text-sm font-medium transition rounded-full bg-zinc-100 py-1 px-3 text-zinc-900 hover:bg-zinc-200 dark:bg-zinc-800/40 dark:text-zinc-400 dark:ring-1 dark:ring-inset dark:ring-zinc-800 dark:hover:bg-zinc-800 dark:hover:text-zinc-300"
-          aria-label="Next: Quickstart"
-          href="/quickstart"
+          navigate={@previous_link}
         >
-          Next<svg viewBox="0 0 20 20" fill="none" aria-hidden="true" class="mt-0.5 h-5 w-5 -mr-1"><path
+          <svg
+            viewBox="0 0 20 20"
+            fill="none"
+            class="mt-0.5 h-5 w-5 -ml-1 rotate-180"
+          ><path
               stroke="currentColor"
               stroke-linecap="round"
               stroke-linejoin="round"
               d="m11.5 6.5 3 3.5m0 0-3 3.5m3-3.5h-9"
-            ></path></svg>
-        </a>
-        <a
-          tabindex="-1"
-          aria-hidden="true"
+            ></path></svg>Previous
+        </.link><.link
           class="text-base font-semibold text-zinc-900 transition hover:text-zinc-600 dark:text-white dark:hover:text-zinc-300"
-          href="/quickstart"
-        >
-          Quickstart
-        </a>
+          navigate={@previous_link}
+        >Page <%= @page_number - 1 %></.link>
       </div>
+      <% end %>
+
+      <%= if assigns.page_number !== assigns.total_pages do %>
+        <div class="ml-auto flex flex-col items-end gap-3">
+          <.link
+            class="inline-flex gap-0.5 justify-center overflow-hidden text-sm font-medium transition rounded-full bg-zinc-100 py-1 px-3 text-zinc-900 hover:bg-zinc-200 dark:bg-zinc-800/40 dark:text-zinc-400 dark:ring-1 dark:ring-inset dark:ring-zinc-800 dark:hover:bg-zinc-800 dark:hover:text-zinc-300"
+            navigate={@next_link}
+          >
+            Next<svg viewBox="0 0 20 20" fill="none" aria-hidden="true" class="mt-0.5 h-5 w-5 -mr-1"><path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="m11.5 6.5 3 3.5m0 0-3 3.5m3-3.5h-9"
+              ></path></svg>
+          </.link><.link
+            class="text-base font-semibold text-zinc-900 transition hover:text-zinc-600 dark:text-white dark:hover:text-zinc-300"
+            navigate={@next_link}
+          >Page <%= @page_number + 1 %></.link>
+        </div>
+      <% end %>
     </div>
     """
   end
