ddadf6a075cc613135fd221897bedfc0bf7adaa1
trueChazza <chaz.goodare@gmail.com>
2023-09-11 15:00:27 +1200

    refactor: refactor layout

--- lib/media_server_web/live/history_live/index.ex
+++ lib/media_server_web/live/history_live/index.ex
@@ -16,29 +16,6 @@ defmodule MediaServerWeb.HistoryLive.Index do
   end
 
   @impl true
-  def handle_params(%{"filter_by" => "episodes"}, _url, socket) do
-    query =
-      from ma in MediaServer.MediaActions,
-        left_join: continue in MediaServer.Continues,
-        on:
-          ma.media_id == continue.media_id and ma.media_type_id == continue.media_type_id and
-            continue.user_id == ^socket.assigns.current_user.id,
-        where: ma.media_type_id == ^MediaServer.MediaTypes.get_episode_id(),
-        order_by: [desc: ma.updated_at],
-        limit: 10,
-        preload: [continue: continue]
-
-    current_user = socket.assigns.current_user |> MediaServer.Repo.preload(media_actions: query)
-
-    {
-      :noreply,
-      socket
-      |> assign(page_title: "History - Episodes")
-      |> assign(media_type: "episodes")
-      |> assign(:user_media_actions, current_user.media_actions)
-    }
-  end
-
   def handle_params(_params, _url, socket) do
     query =
       from ma in MediaServer.MediaActions,
