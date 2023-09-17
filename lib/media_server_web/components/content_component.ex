ff10beb57efe1334cfc1a883fa2d3abf3560bdaf
trueChazza <chaz.goodare@gmail.com>
2023-09-17 15:13:56 +1200

    refactor: remove unused components

--- lib/media_server_web/components/content_component.ex
+++
@@ -1,8 +0,0 @@
-defmodule MediaServerWeb.Components.ContentComponent do
-  use MediaServerWeb, :live_component
-
-  @impl true
-  def preload(list_of_assigns) do
-    list_of_assigns
-  end
-end
--- lib/media_server_web/components/side_bar_component.ex
+++
@@ -1,3 +0,0 @@
-defmodule MediaServerWeb.Components.SideBarComponent do
-  use MediaServerWeb, :live_component
-end
