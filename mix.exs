388a2355d04ba68611f594992f9cedcf43540fec
trueChazza <chaz.goodare@gmail.com>
2023-10-02 12:17:44 +1300

    build: remove cors

--- mix.exs
+++ mix.exs
@@ -55,7 +55,6 @@ defmodule MediaServer.MixProject do
       {:httpoison, "~> 2.0"},
       {:excoveralls, "~> 0.10", only: :test, runtime: false},
       {:tailwind, "~> 0.1", runtime: Mix.env() == :dev},
-      {:cors_plug, "~> 3.0"},
       {:scrivener, "~> 2.0"},
       {:extitles, "~> 0.1.0"},
       {:exstream, "~> 0.20.0"},
