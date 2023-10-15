9b787852f78bc731ea6943b61075641410ee35d0
trueChazza <chaz.goodare@gmail.com>
2023-10-15 15:36:26 +1300

    refactor: use task series actions

--- lib/media_server/series_actions.ex
+++ lib/media_server/series_actions.ex
@@ -1,33 +1,33 @@
 defmodule MediaServer.SeriesActions do
-  use GenServer
+  use Task
 
   require Logger
 
-  def start_link(_args) do
-    GenServer.start_link(__MODULE__, %{})
+  def start_link(arg) do
+    Task.start_link(__MODULE__, :run, [arg])
   end
 
-  def init(state) do
+  def run(_arg) do
     Phoenix.PubSub.subscribe(MediaServer.PubSub, "series")
-
-    {:ok, state}
   end
 
-  def handle_info({:added}, state) do
+  def handle_info({:added, %{"series" => %{"id" => id, "title" => title}}}) do
     MediaServer.SeriesIndex.reset()
 
-    {:noreply, state}
+    followers = MediaServer.MediaActions.series(id) |> MediaServer.MediaActions.followers()
+
+      Enum.each(followers, fn media_action ->
+        Enum.each(media_action.user.push_subscriptions, fn push_subscription ->
+          WebPushElixir.send_notification(push_subscription.push_subscription, "#{ title } is now available")
+        end)
+      end)
   end
 
-  def handle_info({:deleted}, state) do
+  def handle_info({:deleted}) do
     MediaServer.SeriesIndex.reset()
-
-    {:noreply, state}
   end
 
-  def handle_info({:deleted_episode_file}, state) do
+  def handle_info({:deleted_episode_file}) do
     MediaServer.SeriesIndex.reset()
-
-    {:noreply, state}
   end
 end
--- lib/media_server_web/controllers/webhooks_controller.ex
+++ lib/media_server_web/controllers/webhooks_controller.ex
@@ -30,16 +30,8 @@ defmodule MediaServerWeb.WebhooksController do
     |> send_resp(201, "Ok")
   end
 
-  def create(conn, %{"id" => "series", "eventType" => "Download", "series" => %{"id" => id, "title" => title}}) do
-    MediaServer.SeriesIndex.reset()
-
-    followers = MediaServer.MediaActions.series(id) |> MediaServer.MediaActions.followers()
-
-    Enum.each(followers, fn media_action ->
-      Enum.each(media_action.user.push_subscriptions, fn push_subscription ->
-        WebPushElixir.send_notification(push_subscription.push_subscription, "#{ title } is now available")
-      end)
-    end)
+  def create(conn, %{"id" => "series", "eventType" => "Download"} = params) do
+    Phoenix.PubSub.broadcast(MediaServer.PubSub, "series", {:added, params})
 
     conn
     |> send_resp(201, "Ok")
