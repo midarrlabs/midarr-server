e0a719c449c8d9eaae501707bf4a1b70d537b1d5
trueChazza <chaz.goodare@gmail.com>
2023-10-17 17:51:08 +1300

    refactor: refactor web helpers

--- lib/media_server_web/helpers.ex
+++ lib/media_server_web/helpers.ex
@@ -1,8 +1,4 @@
 defmodule MediaServerWeb.Helpers do
-  def minutes_remaining_from_seconds(duration, current_time) do
-    ceil((duration - current_time) / 60)
-  end
-
   def percentage_complete_from_seconds(current_time, duration) do
     current_time / duration * 100
   end
