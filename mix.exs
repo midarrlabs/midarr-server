fdb304fefcc1f2ab421d485af301d69fbd6fa75d
trueChazza <chaz.goodare@gmail.com>
2024-03-03 15:31:52 +1300

    build: add genstage

--- mix.exs
+++ mix.exs
@@ -61,7 +61,8 @@ defmodule MediaServer.MixProject do
       {:web_push_elixir, "~> 0.4.0"},
       {:oapi_tmdb, "~> 0.3.0"},
       {:hls_playlist, "~> 0.2.1"},
-      {:exile, "~> 0.9.1"}
+      {:exile, "~> 0.9.1"},
+      {:gen_stage, "~> 1.0"}
     ]
   end
 
