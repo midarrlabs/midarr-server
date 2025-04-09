73933a555f977926610083d76e3fb5bb1605cc26
trueChazza <chaz.goodare@gmail.com>
2025-04-09 15:45:03 +1200

    build: add mix

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule MediaServer.MixProject do
     [
       app: :media_server,
       version: "0.1.0",
-      elixir: "~> 1.15",
+      elixir: "~> 1.18",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
