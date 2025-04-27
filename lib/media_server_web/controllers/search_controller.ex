da24fb406c62eb9b1ab08579874f60655f8d9829
trueChazza <chaz.goodare@gmail.com>
2025-04-28 10:57:29 +1200

    refactor: search api

--- lib/media_server_web/controllers/search_controller.ex
+++ lib/media_server_web/controllers/search_controller.ex
@@ -1,34 +1,41 @@
 defmodule MediaServerWeb.SearchController do
   use MediaServerWeb, :controller
 
-  def index(conn, %{"query" => query}) do
-    movies =
-      Scrivener.paginate(
-        MediaServer.MoviesIndex.search(MediaServer.MoviesIndex.all(), query),
-        %{
-          "page" => "1",
-          "page_size" => "50"
-        }
-      )
+  import Ecto.Query
+
+  def index(conn, %{"query" => query} = params) do
+    movies_query = MediaServer.Movies |> where([m], ilike(m.title, ^"%#{query}%"))
+    series_query = MediaServer.Series |> where([s], ilike(s.title, ^"%#{query}%"))
+
+    {:ok, {movies, movies_meta}} = Flop.validate_and_run(movies_query, params)
+    {:ok, {series, series_meta}} = Flop.validate_and_run(series_query, params)
 
     conn
-    |> put_resp_header("content-type", "application/json")
-    |> send_resp(
-         200,
-         Jason.encode!(%{
-           "total" => movies.total_entries,
-           "items" =>
-             Enum.map(movies.entries, fn x ->
-                                         %{
-                                           "title" => x["title"],
-                                           "overview" => x["overview"],
-                                           "year" => x["year"],
-                                           "poster" => ~p"/api/images?movie=#{x["id"]}&type=poster",
-                                           "background" => ~p"/api/images?movie=#{x["id"]}&type=background",
-                                           "stream" => ~p"/api/stream?movie=#{x["id"]}"
-                                         }
-             end)
-         })
-       )
+    |> json(%{
+      data: %{
+        movies: movies,
+        series: series,
+      },
+      meta: %{
+        movies: %{
+          total_pages: movies_meta.total_pages,
+          total_count: movies_meta.total_count,
+          has_next_page: movies_meta.has_next_page?,
+          has_previous_page: movies_meta.has_previous_page?,
+          previous_page: movies_meta.previous_page,
+          next_page: movies_meta.next_page,
+          current_page: movies_meta.current_page
+        },
+        series: %{
+          total_pages: series_meta.total_pages,
+          total_count: series_meta.total_count,
+          has_next_page: series_meta.has_next_page?,
+          has_previous_page: series_meta.has_previous_page?,
+          previous_page: series_meta.previous_page,
+          next_page: series_meta.next_page,
+          current_page: series_meta.current_page
+        }
+      }
+    })
   end
 end
